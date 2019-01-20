8b79c6f4230a7c4ab33174a1749e0407b1dab5e9
kuru <amohkuru@gmail.com>
2019-01-21 04:12:57 +0900

    update

---
+++ Union_Find_Tree.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Union Find Tree
+class Union_Find_Tree {
+public:
+	vector<int> parent;
+    vector<int> rank;
+
+    Union_Find_Tree(int N = 1) : parent(N), rank(N,0){
+		for (int i = 0; i < N; ++i) parent[i] = i;
+	}
+ 
+	int root(int n) {
+		return (parent[n] == n ? n : parent[n] = root(parent[n]));
+	}
+
+    bool same(int x, int y) {
+		return root(x) == root(y);
+	}
+ 
+	void unite(int n, int m) {
+		n = root(n);
+		m = root(m);
+		if (n == m) return;
+		if(rank[n]<rank[m]) {
+            parent[n] = m;
+        }
+        else{
+            parent[m] = n;
+            if(rank[n] == rank[m]) rank[n]++;
+        }
+	}
+};
+
+//verify https://atcoder.jp/contests/abc097/tasks/arc097_b
