cace879648c9e76fdbbeb581b22765538b1f7a13
kuru <amohkuru@gmail.com>
2019-05-18 19:34:52 +0900

    update

--- Distance.cpp
+++ Distance.cpp
@@ -14,7 +14,11 @@ template<class T> T ChebDist(T x1, T y1, T x2, T y2) {
 //Manhattan distance
 template<class T> T ManhDist(T x1, T y1, T x2, T y2) {
     return abs(x1 - x2)+abs(y1 - y2);
-}	
+}
 
+double Distance_Point_to_Line(const double& x,const double& y,const double& x1,const double& y1,const double& x2,const double& y2){
+	double a = y2 - y1, b = -(x2 - x1), c = -(y2-y1)*x1+(x2-x1)*y1;
+	return abs(a*x+b*y+c)/sqrt(a*a+b*b);
+}
 
 //verify 
