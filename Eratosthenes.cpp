b77cc3fe5dcfa4992d3440cf211c3304bbab4243
kuru <amohkuru@gmail.com>
2019-09-22 01:48:36 +0900

    エラトステネスの型をboolからintに変更

--- Eratosthenes.cpp
+++ Eratosthenes.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 //Eratosthenes O(N)
-vector<bool> Eratosthenes(int N) {
-    vector<bool> res(N + 1, true);
-    res[0] = res[1%(N+1)] = false;
-    for (int i = 1; i*i <= N; i++) if (res[i]) for (int j = 2 * i; j <= N; j += i) res[j] = false;
+vector<int> Eratosthenes(int N) {
+    vector<int> res(N + 1, 1);
+    res[0] = res[1%(N+1)] = 0;
+    for (int i = 1; i*i <= N; i++) if (res[i]) for (int j = 2 * i; j <= N; j += i) res[j] = 0;
     return res;
 }
 
