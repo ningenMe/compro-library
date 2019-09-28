bbb227c2678fa4349f203f7ba1b67bb1272d828e
kuru <amohkuru@gmail.com>
2019-09-29 03:54:13 +0900

    命名をきれいに

--- FactorialMod.cpp
+++ FactorialMod.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//Factorial_Mod
-vector<long long> Factorial_Mod(long long N, long long mod) {
+//Factorial Mod
+vector<long long> FactorialMod(long long N, long long mod) {
     vector<long long> res(N + 1, 1);
     for (long long i = 1; i <= N; ++i) res[i] = (res[i - 1] * i) % mod;
     return res;
