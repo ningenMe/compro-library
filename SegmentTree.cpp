12b02352ee35ee537a03bc6d64085dd4e596e4e3
kuru <amohkuru@gmail.com>
2019-09-29 03:54:00 +0900

    SegmentTree　Mergeの抽象化

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -1,20 +1,21 @@
 template<class T> class SegmentTree {
-	size_t length; //セグメント木の最下段の要素の数(vectorの要素の数を超える2べきの数)
-	T init;         //各ノードの初期値
-	vector<T> node; //ノード
-	function<T(T,T)> func;
+	size_t length;               //セグメント木の最下段の要素の数(vectorの要素の数を超える2べきの数)
+	T init;                      //各ノードの初期値
+	vector<T> node;              //ノード
+	function<T(T,T)> funcNode;   //node同士の演算
+	function<T(T,T)> funcMerge;  //nodeとvarの演算
 
 public:
 	//vectorで初期化
-	SegmentTree(const vector<T> & vec, const T init, function<T(T,T)> func) : init(init), func(func) {
+	SegmentTree(const vector<T> & vec, const T init, function<T(T,T)> funcNode, function<T(T,T)> funcMerge) : init(init), funcNode(funcNode), funcMerge(funcMerge) {
 		for (length = 1; length < vec.size(); length *= 2);
 		node.resize(2 * length, init);
 		for (int i = 0; i < vec.size(); ++i) node[i + length] = vec[i];
-		for (int i = length - 1; i >= 0; --i) node[i] = func(node[(i<<1)+0],node[(i<<1)+1]);
+		for (int i = length - 1; i >= 0; --i) node[i] = funcNode(node[(i<<1)+0],node[(i<<1)+1]);
 	}
  
 	//lengthだけで初期化
-	SegmentTree(const size_t num, const T init, function<T(T,T)> func) : init(init), func(func) {
+	SegmentTree(const size_t num, const T init, function<T(T,T)> funcNode, function<T(T,T)> funcMerge) : init(init), funcNode(funcNode), funcMerge(funcMerge) {
 		for (length = 1; length < num; length *= 2);
 		node.resize(2 * length, init);
 	}
@@ -23,16 +24,8 @@ public:
 	void update(size_t idx, const T var) {
 		if(idx < 0 || length <= idx) return;
 		idx += length;
-		node[idx] = var;
-		while(idx >>= 1) node[idx] = func(node[(idx<<1)+0],node[(idx<<1)+1]);
-	}
-
-	//idx : 0-indexed
-	void add(size_t idx, const T var) {
-		if(idx < 0 || length <= idx) return;
-		idx += length;
-		node[idx] += var;
-		while(idx >>= 1) node[idx] = func(node[(idx<<1)+0],node[(idx<<1)+1]);
+		node[idx] = funcMerge(node[idx],var);
+		while(idx >>= 1) node[idx] = funcNode(node[(idx<<1)+0],node[(idx<<1)+1]);
 	}
 
 	//[l,r)
@@ -40,10 +33,10 @@ public:
 		if (l < 0 || length <= l || r < 0 || length < r) return init;
 		T vl = init, vr = init;
 		for(l += length, r += length; l < r; l >>=1, r >>=1) {
-			if(l&1) vl = func(vl,node[l++]);
-			if(r&1) vr = func(vr,node[--r]);
+			if(l&1) vl = funcNode(vl,node[l++]);
+			if(r&1) vr = funcNode(vr,node[--r]);
 		}
-		return func(vl,vr);
+		return funcNode(vl,vr);
 	}
  
 	void print(){
@@ -65,8 +58,20 @@ public:
 //SUM
 //verify https://atcoder.jp/contests/arc077/tasks/arc077_c
 
-//NIN
+//MIN
 //verify https://atcoder.jp/contests/bitflyer2018-final-open/tasks/bitflyer2018_final_c
 
-//NAX
+//MAX
 //verify https://atcoder.jp/contests/wupc2019/tasks/wupc2019_d
+
+	// //一点加算 + 区間総和
+	// function<ll(ll,ll)> funcNode =  [&](ll a,ll b){return a+b;};
+	// function<ll(ll,ll)> funcMerge = [&](ll a,ll b){return a+b;};
+
+	// //一点更新 + 区間GCD
+	// function<ll(ll,ll)> funcNode =  GCD;
+	// function<ll(ll,ll)> funcMerge = [&](ll a,ll b){return b;};
+
+	// //一点更新 + 区間最小
+	// function<ll(ll,ll)> funcNode =  [&](ll a,ll b){return min(a,b);};
+	// function<ll(ll,ll)> funcMerge = [&](ll a,ll b){return b;};
