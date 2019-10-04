9e976fb0b70709636c84328491164753f100c4f5
kuru <amohkuru@gmail.com>
2019-10-05 03:28:48 +0900

    Tree ライブラリ

--- Tree.cpp
+++ Tree.cpp
@@ -1,104 +1,158 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-class Tree {
+template<class T = int> class Tree {
 public:
-	int N, MAX_bit;
+	int nodeNum;
+	int isWeighted;
+	int maxBit;
+	int idx;
 	vector<vector<int>> edge;
+	vector<vector<T>> weight;
 	vector<int> depth;
 	vector<int> order;
-	vector<vector<int>> parent;
+	vector<T> dist;
+	
+	vector<int> parent;
+	vector<T> parentDist;
+	
 	vector<vector<int>> child;
+	vector<vector<T>> childDist;
+ 
+	vector<vector<int>> ancestor;
 	vector<vector<int>> descendant;
-
-
-	Tree(int N, int MAX_bit = 20) :N(N), MAX_bit(MAX_bit), edge(N), depth(N, -1), order(N, -1), parent(N, vector<int>(MAX_bit)), child(N), descendant(N){
-		//do nothing
+	
+	vector<int> eulerTour;
+    vector<T> eulerTourDist;
+	vector<int> idxL;
+	vector<int> idxR;
+ 
+	Tree(const int nodeNum, const int isWeighted = 0, const int maxBit = 20) : 
+	nodeNum(nodeNum),
+	isWeighted(isWeighted),
+	maxBit(maxBit),
+	edge(nodeNum),
+	depth(nodeNum),
+	order(nodeNum)
+	{
+		if(isWeighted) weight.resize(nodeNum);
+		if(isWeighted) dist.resize(nodeNum);
 	}
-
-	//O(1)
-	void make_edge(int from, int to) {
+ 
+	//O(1) anytime
+	void makeEdge(const int& from, const int& to, const T& w = 0) {
 		edge[from].push_back(to);
+		if(isWeighted)	weight[from].push_back(w);
 	}
-
-	//O(N)
-	void make_depth(int root) {
-		queue<int> q;
+ 
+	//O(N) anytime
+	void makeDepth(const int root) {
 		depth[root] = 0;
-		q.push(root);
-		while (q.size()) {
-			int from = q.front();
-			q.pop();
-			for (auto to : edge[from]) {
-				if (depth[to] != -1) continue;
-				depth[to] = depth[from] + 1;
-				q.push(to);
-			}
+		if(isWeighted) dist[root] = 0;
+		idx = 0;
+		dfs1(root);
+		order[idx++] = root;
+	}
+ 
+    //for makeDepth
+	void dfs1(int from, int prev = -1){
+		for(int i = 0; i < edge[from].size(); ++i){
+			int to = edge[from][i];
+			if(to==prev) continue;
+			depth[to] = depth[from] + 1;
+			if(isWeighted) dist[to] = dist[from] + weight[from][i];
+			dfs1(to,from);
+			order[idx++] = to;
 		}
 	}
-
-	//O(NlogN)
-	void make_order(){
-		vector<pair<int,int>> tmp(N);
-		for (int i = 0; i < N; ++i) tmp[i] = {depth[i],i};
-		sort(tmp.begin(),tmp.end(),greater<>());
-		for (int i = 0; i < N; ++i) order[i] = tmp[i].second;
+ 
+    //O(N) anytime
+	int diameter(void){
+		makeDepth(0);
+		int tmp = max_element(depth.begin(), depth.end()) - depth.begin();
+		makeDepth(tmp);
+		return *max_element(depth.begin(), depth.end());
 	}
-
-	//O(MAX_bit*N)
-	void make_parent() {
-		for (int i = 0; i < N; ++i) parent[i][0] = i;
-		for (int i = 0; i < N; ++i) for (auto j : edge[i]) if (depth[i] > depth[j]) parent[i][0] = j;
-		for (int bit = 1; bit < MAX_bit; ++bit) for (int i = 0; i < N; ++i) parent[i][bit] = parent[parent[i][bit - 1]][bit - 1];
+ 
+	//O(N) after makeDepth
+	void makeParent(void) {
+		parent.resize(nodeNum);
+		iota(parent.begin(),parent.end(),0);
+		for (int i = 0; i < nodeNum; ++i) for (auto j : edge[i]) if (depth[i] > depth[j]) parent[i] = j;
+ 
+		if(isWeighted) {
+			parentDist.resize(nodeNum);
+			for (int i = 0; i < nodeNum; ++i) for (int j = 0; j < edge[i].size(); ++j) if (depth[i] > depth[edge[i][j]]) parentDist[i] = weight[i][j];
+		}
 	}
-
-	//O(N)
-	void make_child() {
-		for (int i = 0; i < N; ++i) for (auto j : edge[i]) if (depth[i] < depth[j]) child[i].push_back(j);
+ 
+	//O(N) after makeDepth
+	void makeChild(void) {
+		child.resize(nodeNum);
+		for (int i = 0; i < nodeNum; ++i) for (auto j : edge[i]) if (depth[i] < depth[j]) child[i].push_back(j);
+ 
+		if(isWeighted) {
+			childDist.resize(nodeNum);
+			for (int i = 0; i < nodeNum; ++i) for (int j = 0; j < edge[i].size(); ++j) if (depth[i] < depth[edge[i][j]]) childDist[i].push_back(weight[i][j]);
+		}
 	}
-
-	//O(N^2)
-	void make_descendant() {
-		for (int i = 0; i < N; ++i) descendant[i].push_back(i);
-		for (int i = 0; i < N; ++i) for (auto j : edge[order[i]]) if (depth[order[i]] < depth[j]) for(auto k: descendant[j]) descendant[order[i]].push_back(k);
+ 
+	//O(NlogN) after makeDepth
+	void makeAncestor(void) {
+		ancestor.resize(nodeNum,vector<int>(maxBit));
+		for (int i = 0; i < nodeNum; ++i) ancestor[i][0] = i;
+		for (int i = 0; i < nodeNum; ++i) for (auto j : edge[i]) if (depth[i] > depth[j]) ancestor[i][0] = j;
+		for (int bit = 1; bit < maxBit; ++bit) for (int i = 0; i < nodeNum; ++i) ancestor[i][bit] = ancestor[ancestor[i][bit - 1]][bit - 1];
 	}
-
-	//O(MAX_bit*logN) Lowest_Common_Ancestor
-	int LCA(int l, int r) {
+ 
+	//O(N^2) after makeDepth
+	void makeDescendant(void) {
+		descendant.resize(nodeNum);
+		for (int i = 0; i < nodeNum; ++i) descendant[i].push_back(i);
+		for (int i = 0; i < nodeNum; ++i) for (auto j : edge[order[i]]) if (depth[order[i]] < depth[j]) for(auto k: descendant[j]) descendant[order[i]].push_back(k);
+	}
+ 
+	//O(logN) after makeAncestor
+	int lca(int l, int r) {
 		if (depth[l] < depth[r]) swap(l, r);
 		int diff = depth[l] - depth[r];
-		for (int bit = 0; bit < MAX_bit; ++bit) if (diff & (1 << bit)) l = parent[l][bit];
-		
-		int ok = (1 << MAX_bit), ng = -1, md;
-		while (ok - ng>1) {
-			md = (ok + ng) / 2;
-			int x = l, y = r;
-			for (int bit = 0; bit < MAX_bit; ++bit) if (md & (1 << bit)) x = parent[x][bit];
-			for (int bit = 0; bit < MAX_bit; ++bit) if (md & (1 << bit)) y = parent[y][bit];
-			(x == y ? ok : ng) = md;
-		}
-		for (int bit = 0; bit < MAX_bit; ++bit) if (ok & (1 << bit)) l = parent[l][bit];
-		return l;
+		for (int bit = 0; bit < maxBit; ++bit) if (diff & (1 << bit)) l = ancestor[l][bit];
+		if(l==r) return l;
+		for (int bit = maxBit - 1; 0 <= bit; --bit) if(ancestor[l][bit]!=ancestor[r][bit]) l = ancestor[l][bit], r = ancestor[r][bit];
+		return ancestor[l][0];
 	}
-
-	void print() {
-		cout << " depth { " << depth[0];
-		for (int i = 1; i < N; ++i) cout << ", " << depth[i];
-		cout << " }" << endl;
-
-		cout << "parent { " << parent[0][0];
-		for (int i = 1; i < N; ++i) cout << ", " << parent[i][0];
-		cout << " }" << endl;
-
-		cout << " child { " << child[0];
-		for (int i = 1; i < N; ++i) cout << ", " << child[i];
-		cout << " }" << endl;
-
-		cout << "descen { " << descendant[0];
-		for (int i = 1; i < N; ++i) cout << ", " << descendant[i];
-		cout << " }" << endl;
+ 
+	//O(N) after makeChild and makeParent
+	void makeEulerTour(void){
+        dfs2(order[nodeNum-1]);
+		idxL.resize(nodeNum);
+		idxR.resize(nodeNum);
+		for(int i = 0; i < eulerTour.size(); ++i) idxR[eulerTour[i]] = i;
+		for(int i = eulerTour.size()-1; 0 <= i; --i) idxL[eulerTour[i]] = i;
+		return;
 	}
-
+ 
+    //for makeEulerTour
+	void dfs2(int from, int prev = -1){
+		eulerTour.push_back(from);
+        if(isWeighted) eulerTourDist.push_back(parentDist[from]);
+ 
+        for(int i = 0; i < child[from].size(); ++i){
+            int to = child[from][i];            
+            dfs2(to,from);            
+    		eulerTour.push_back(from);
+            if(isWeighted) eulerTourDist.push_back(-childDist[from][i]);
+        }
+	}
+ 
 };
+ 
+//depth,dist
+//https://atcoder.jp/contests/abc126/tasks/abc126_d
+ 
+//lca
+//https://atcoder.jp/contests/abc014/tasks/abc014_4
+ 
+//child
+//https://atcoder.jp/contests/abc133/tasks/abc133_e
 
-//verify https://atcoder.jp/contests/abc014/tasks/abc014_4
+//eulerTour
+//https://yukicoder.me/problems/no/900
+
