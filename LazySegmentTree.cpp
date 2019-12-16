3326e0eed28ffa902cce3aa90955fdc1d6504fba
kuru <amohkuru@gmail.com>
2019-12-17 02:21:05 +0900

    遅延セグ木、インターフェースの改良

--- LazySegmentTree.cpp
+++ LazySegmentTree.cpp
@@ -1,45 +1,41 @@
-template<class typeNode, class typeLazy, class Operator> class LazySegmentTree {
+template<class Operator> class LazySegmentTree {
+    Operator Op;                                         //Operator 演算子、型、単位元を持つ
+	using typeNode = decltype(Op.unitNode);              //nodeの型
+	using typeLazy = decltype(Op.unitLazy);              //lazyの型
 	size_t length;                                       //セグメント木の最下段の要素の数(vectorの要素の数を超える2べきの数)
 	size_t height;                                       //セグメント木の高さ 
-	typeNode unitNode;                                   //nodeの単位元
-	typeLazy unitLazy;                                   //lazyの単位元
 	vector<typeNode> node;                               //node
 	vector<typeLazy> lazy;                               //lazy
 	vector<int> width;                                   //width
-    Operator Op;
-
 public:
 
-	//同じinitで初期化
-	LazySegmentTree(const size_t num, const typeNode unitNode, const typeLazy unitLazy, const typeNode init)
-    : unitNode(unitNode), unitLazy(unitLazy) {
+	//unitで初期化
+	LazySegmentTree(const size_t num) {
 		for (length = 1,height = 0; length < num; length *= 2, height++);
-		node.resize(2 * length, unitNode);
-		lazy.resize(2 * length, unitLazy);
+		node.resize(2 * length, Op.unitNode);
+		lazy.resize(2 * length, Op.unitLazy);
+		width.resize(2 * length, 0);
+		for (int i = 0; i < num; ++i) node[i + length] = Op.unitNode;
+		for (int i = length - 1; i >= 0; --i) node[i] = Op.funcNode(node[(i<<1)+0],node[(i<<1)+1]);
+		for(int i = length; i < 2*length; ++i) for(int j = i, k = 1; j && !width[j] ; j >>= 1,k <<= 1) width[j] = k;
+	}
+
+	// //同じinitで初期化
+	LazySegmentTree(const size_t num, const typeNode init) {
+		for (length = 1,height = 0; length < num; length *= 2, height++);
+		node.resize(2 * length, Op.unitNode);
+		lazy.resize(2 * length, Op.unitLazy);
 		for (int i = 0; i < num; ++i) node[i + length] = init;
 		for (int i = length - 1; i >= 0; --i) node[i] = Op.funcNode(node[(i<<1)+0],node[(i<<1)+1]);
 		width.resize(2 * length, 0);
 		for(int i = length; i < 2*length; ++i) for(int j = i, k = 1; j && !width[j] ; j >>= 1,k <<= 1) width[j] = k;
 	}
 
-	//unitで初期化
-	LazySegmentTree(const size_t num, const typeNode unitNode, const typeLazy unitLazy)
-    : unitNode(unitNode), unitLazy(unitLazy) {
-		for (length = 1,height = 0; length < num; length *= 2, height++);
-		node.resize(2 * length, unitNode);
-		lazy.resize(2 * length, unitLazy);
-		for (int i = 0; i < num; ++i) node[i + length] = unitNode;
-		for (int i = length - 1; i >= 0; --i) node[i] = Op.funcNode(node[(i<<1)+0],node[(i<<1)+1]);
-		width.resize(2 * length, 0);
-		for(int i = length; i < 2*length; ++i) for(int j = i, k = 1; j && !width[j] ; j >>= 1,k <<= 1) width[j] = k;
-	}
-
 	//vectorで初期化
-	LazySegmentTree(const vector<typeNode>& vec, const typeNode unitNode, const typeLazy unitLazy)
-	 : unitNode(unitNode), unitLazy(unitLazy) {
+	LazySegmentTree(const vector<typeNode>& vec) {
 		for (length = 1,height = 0; length < vec.size(); length *= 2, height++);
-		node.resize(2 * length, unitNode);
-		lazy.resize(2 * length, unitLazy);
+		node.resize(2 * length, Op.unitNode);
+		lazy.resize(2 * length, Op.unitLazy);
 		for (int i = 0; i < vec.size(); ++i) node[i + length] = vec[i];
 		for (int i = length - 1; i >= 0; --i) node[i] = Op.funcNode(node[(i<<1)+0],node[(i<<1)+1]);
 		width.resize(2 * length, 0);
@@ -48,11 +44,11 @@ public:
 
 
 	void propagate(int k) {
-		if(lazy[k] == unitLazy) return;
+		if(lazy[k] == Op.unitLazy) return;
 		node[k] = Op.funcMerge(node[k],lazy[k],width[k]);
 		if(k < length) lazy[2*k+0] = Op.funcLazy(lazy[2*k+0],lazy[k]);
 		if(k < length) lazy[2*k+1] = Op.funcLazy(lazy[2*k+1],lazy[k]);
-		lazy[k] = unitLazy;
+		lazy[k] = Op.unitLazy;
     }
 
 
@@ -66,8 +62,8 @@ public:
 		}
 		l = a + length, r = b + length - 1;
 		while ((l>>=1),(r>>=1),l) {
-			if(lazy[l] == unitLazy) node[l] = Op.funcNode(Op.funcMerge(node[(l<<1)+0],lazy[(l<<1)+0],width[(l<<1)+0]),Op.funcMerge(node[(l<<1)+1],lazy[(l<<1)+1],width[(l<<1)+1]));
-			if(lazy[r] == unitLazy) node[r] = Op.funcNode(Op.funcMerge(node[(r<<1)+0],lazy[(r<<1)+0],width[(r<<1)+0]),Op.funcMerge(node[(r<<1)+1],lazy[(r<<1)+1],width[(r<<1)+1]));
+			if(lazy[l] == Op.unitLazy) node[l] = Op.funcNode(Op.funcMerge(node[(l<<1)+0],lazy[(l<<1)+0],width[(l<<1)+0]),Op.funcMerge(node[(l<<1)+1],lazy[(l<<1)+1],width[(l<<1)+1]));
+			if(lazy[r] == Op.unitLazy) node[r] = Op.funcNode(Op.funcMerge(node[(r<<1)+0],lazy[(r<<1)+0],width[(r<<1)+0]),Op.funcMerge(node[(r<<1)+1],lazy[(r<<1)+1],width[(r<<1)+1]));
 		}
     }
 
@@ -75,7 +71,7 @@ public:
 	typeNode get(int a, int b) {
 		int l = a + length, r = b + length - 1;
 		for (int i = height; 0 < i; --i) propagate(l >> i), propagate(r >> i);
-		typeNode vl = unitNode, vr = unitNode;
+		typeNode vl = Op.unitNode, vr = Op.unitNode;
 		for(r++; l < r; l >>=1, r >>=1) {
 			if(l&1) vl = Op.funcNode(vl,Op.funcMerge(node[l],lazy[l],width[l])),l++;
 			if(r&1) r--,vr = Op.funcNode(Op.funcMerge(node[r],lazy[r],width[r]),vr);
@@ -84,136 +80,101 @@ public:
 	}
 
 	void print(){
-		cout << "node" << endl;
-		for(int i = 1,j = 1; i < 2*length; ++i) {
-			cout << node[i] << " ";
-			if(i==((1<<j)-1) && ++j) cout << endl;
-		}
-		cout << "lazy" << endl;
-		for(int i = 1,j = 1; i < 2*length; ++i) {
-			cout << lazy[i] << " ";
-			if(i==((1<<j)-1) && ++j) cout << endl;
-		}
+		// cout << "node" << endl;
+		// for(int i = 1,j = 1; i < 2*length; ++i) {
+		// 	cout << node[i] << " ";
+		// 	if(i==((1<<j)-1) && ++j) cout << endl;
+		// }
+		// cout << "lazy" << endl;
+		// for(int i = 1,j = 1; i < 2*length; ++i) {
+		// 	cout << lazy[i] << " ";
+		// 	if(i==((1<<j)-1) && ++j) cout << endl;
+		// }
 		// cout << "width" << endl;
 		// for(int i = 1,j = 1; i < 2*length; ++i) {
 		// 	cout << width[i] << " ";
 		// 	if(i==((1<<j)-1) && ++j) cout << endl;
 		// }
-		// cout << "vector" << endl;
-		// cout << "{ " << get(0,1);
-		// for(int i = 1; i < length; ++i) cout << ", " << get(i,i+1);
-		// cout << " }" << endl;
+		cout << "vector" << endl;
+		cout << "{ " << get(0,1);
+		for(int i = 1; i < length; ++i) cout << ", " << get(i,i+1);
+		cout << " }" << endl;
 	}
 
 };
 
 //node:総和　lazy:加算
 template<class typeNode, class typeLazy> struct nodeSumLazyAdd {
-	typeNode funcNode(typeNode l,typeNode r){
-        return l+r;
-    }
-	typeLazy funcLazy(typeLazy l,typeLazy r){
-        return l+r;
-    }
-	typeNode funcMerge(typeNode l,typeLazy r,int len){
-        return l+r*len;
-    }
-    //example
-    //LazySegmentTree<ll,ll,nodeSumLazyPlus<ll,ll>> Seg(N,0,0,0);
+	typeNode unitNode = 0;
+	typeLazy unitLazy = 0;
+	typeNode funcNode(typeNode l,typeNode r){return l+r;}
+	typeLazy funcLazy(typeLazy l,typeLazy r){return l+r;}
+	typeNode funcMerge(typeNode l,typeLazy r,int len){return l+r*len;}
+    // LazySegmentTree<nodeSumLazyPlus<ll,ll>> Seg(N,0);
 };
 
 //node:最小　lazy:加算
 template<class typeNode, class typeLazy> struct nodeMinLazyAdd {
-	typeNode funcNode(typeNode l,typeNode r){
-        return min(l,r);
-    }
-	typeLazy funcLazy(typeLazy l,typeLazy r){
-        return l+r;
-    }
-	typeNode funcMerge(typeNode l,typeLazy r,int len){
-        return l+r;
-    }
-    //example
-    //LazySegmentTree<ll,ll,nodeMinLazyAdd<ll,ll>> Seg(N,inf,0,0);
+	typeNode unitNode = 1234567890;
+	typeLazy unitLazy = 0;
+	typeNode funcNode(typeNode l,typeNode r){return min(l,r);}
+	typeLazy funcLazy(typeLazy l,typeLazy r){return l+r;}
+	typeNode funcMerge(typeNode l,typeLazy r,int len){return l+r;}
+    // LazySegmentTree<nodeMinLazyAdd<ll,ll>> Seg(N,0);
 	//verify https://onlinejudge.u-aizu.ac.jp/problems/DSL_2_H
+};
+
+//node:mod和　lazy:mod加算
+template<class typeNode, class typeLazy> struct nodeModSumLazyModAdd {
+	typeNode unitNode = 0;
+	typeLazy unitLazy = 0;
+	typeNode funcNode(typeNode l,typeNode r){return (l+r)%MOD;}
+	typeLazy funcLazy(typeLazy l,typeLazy r){return (l+r)%MOD;}
+	typeNode funcMerge(typeNode l,typeLazy r,int len){return (l+(r*len)%MOD)%MOD;}
+	// LazySegmentTree<nodeModSumLazyModAdd<ll,ll>> dp(N+3,0);
+    //verify https://atcoder.jp/contests/code-festival-2018-quala/tasks/code_festival_2018_quala_d
+};
+
+//node:最大　lazy:更新
+template<class typeNode, class typeLazy> struct nodeMaxLazyUpdate {
+	typeNode unitNode = 0;
+	typeLazy unitLazy = 0;
+	typeNode funcNode(typeNode l,typeNode r){return max(l,r);}
+	typeLazy funcLazy(typeLazy l,typeLazy r){return r;}
+	typeNode funcMerge(typeNode l,typeLazy r,int len){return r!=0?r*len:l;}
+    // LazySegmentTree<nodeMaxLazyUpdate<ll,ll>> Seg(N+2,0);
+    //verify https://atcoder.jp/contests/nikkei2019-final/tasks/nikkei2019_final_d
+};
+
+//node:最小　lazy:更新
+template<class typeNode, class typeLazy> struct nodeMinLazyUpdate {
+	typeNode unitNode = 12345678910;
+	typeLazy unitLazy = 0;
+	typeNode funcNode(typeNode l,typeNode r){return min(l,r);}
+	typeLazy funcLazy(typeLazy l,typeLazy r){return r;}
+	typeNode funcMerge(typeNode l,typeLazy r,int len){return r!=0?r*len:l;}
+	//LazySegmentTree<ll,ll,nodeMinLazyUpdate<ll,ll>> Seg(N,HIGHINF,0);
+    //verify https://atcoder.jp/contests/code-festival-2018-final/tasks/code_festival_2018_final_e
 };
 
 //node:GCD　lazy:更新
 template<class typeNode, class typeLazy> struct nodeGCDLazyUpdate {
-	typeNode funcNode(typeNode l,typeNode r){
-        return ((r == 0) ? l : funcNode(r, l % r));
-    }
-	typeLazy funcLazy(typeLazy l,typeLazy r){
-        return r;
-    }
-	typeNode funcMerge(typeNode l,typeLazy r,int len){
-        return r!=0?r:l;
-    }
-    //example
-    //LazySegmentTree<ll,ll,nodeGCDLazyUpdate<ll,ll>> Seg(A,0,0);
+	typeNode unitNode = 0;
+	typeLazy unitLazy = 0;
+	typeNode funcNode(typeNode l,typeNode r){return ((r == 0) ? l : funcNode(r, l % r));}
+	typeLazy funcLazy(typeLazy l,typeLazy r){return r;}
+	typeNode funcMerge(typeNode l,typeLazy r,int len){return r!=0?r:l;}
+    // LazySegmentTree<nodeGCDLazyUpdate<ll,ll>> Seg(A);
     //verify https://atcoder.jp/contests/abc125/tasks/abc125_c
 };
 
 //node:総和　lazy:更新
 template<class typeNode, class typeLazy> struct nodeSumLazyUpdate {
-	typeNode funcNode(typeNode l,typeNode r){
-        return l+r;
-    }
-	typeLazy funcLazy(typeLazy l,typeLazy r){
-        return r;
-    }
-	typeNode funcMerge(typeNode l,typeLazy r,int len){
-        return r!=-2000?r*len:l;
-    }
-    //example
-	//LazySegmentTree<ll,ll,nodeSumLazyUpdate<ll,ll>> Seg(N,0,-2000,0);
+	typeNode unitNode = 0;
+	typeLazy unitLazy = -2000;
+	typeNode funcNode(typeNode l,typeNode r){return l+r;}
+	typeLazy funcLazy(typeLazy l,typeLazy r){return r;}
+	typeNode funcMerge(typeNode l,typeLazy r,int len){return r!=-2000?r*len:l;}
+	// LazySegmentTree<nodeSumLazyUpdate<ll,ll>> Seg(N,0,-2000,0);
     //verify https://onlinejudge.u-aizu.ac.jp/problems/DSL_2_I
-};
-
-//node:最大　lazy:更新
-template<class typeNode, class typeLazy> struct nodeMaxLazyUpdate {
-	typeNode funcNode(typeNode l,typeNode r){
-        return max(l,r);
-    }
-	typeLazy funcLazy(typeLazy l,typeLazy r){
-        return r;
-    }
-	typeNode funcMerge(typeNode l,typeLazy r,int len){
-        return r!=0?r*len:l;
-    }
-    //example
-	//LazySegmentTree<ll,ll,nodeMaxLazyUpdate<ll,ll>> Seg(N+2,0,0);
-    //verify https://atcoder.jp/contests/nikkei2019-final/tasks/nikkei2019_final_d
-};
-
-//node:最大　lazy:更新
-template<class typeNode, class typeLazy> struct nodeMinLazyUpdate {
-	typeNode funcNode(typeNode l,typeNode r){
-        return min(l,r);
-    }
-	typeLazy funcLazy(typeLazy l,typeLazy r){
-        return r;
-    }
-	typeNode funcMerge(typeNode l,typeLazy r,int len){
-        return r!=0?r*len:l;
-    }
-    //example
-	//LazySegmentTree<ll,ll,nodeMinLazyUpdate<ll,ll>> Seg(N,HIGHINF,0);
-    //verify https://atcoder.jp/contests/code-festival-2018-final/tasks/code_festival_2018_final_e
-};
-
-//node:mod和　lazy:mod加算
-template<class typeNode, class typeLazy> struct nodeModSumLazyModAdd {
-	typeNode funcNode(typeNode l,typeNode r){
-        return (l+r)%MOD;
-    }
-	typeLazy funcLazy(typeLazy l,typeLazy r){
-        return (l+r)%MOD;
-    }
-	typeNode funcMerge(typeNode l,typeLazy r,int len){
-        return (l+(r*len)%MOD)%MOD;
-    }
-    //example
-	//LazySegmentTree<ll,ll,nodeMinLazyUpdate<ll,ll>> Seg(N,HIGHINF,0);
-    //verify https://atcoder.jp/contests/code-festival-2018-quala/tasks/code_festival_2018_quala_d
 };
