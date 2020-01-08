b556dc55b7dfff78a7aa50bf3e1008fbea1d36be
ningenme <amohkuru@gmail.com>
2020-01-09 00:46:04 +0900

    SegmentTree function脱却

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -1,42 +1,49 @@
-template<class T> class SegmentTree {
-	size_t length;               //セグメント木の最下段の要素の数(vectorの要素の数を超える2べきの数)
-	T init;                      //各ノードの初期値
-	vector<T> node;              //ノード
-	function<T(T,T)> funcNode;   //node同士の演算
-	function<T(T,T)> funcMerge;  //nodeとvarの演算
+
+template<class Operator> class SegmentTree {
+	Operator Op;                            //Operator 演算子、型、単位元を持つ
+	using typeNode = decltype(Op.unitNode); //nodeの型
+	size_t length;                          //セグメント木の最下段の要素の数(vectorの要素の数を超える2べきの数)
+	vector<typeNode> node;                  //ノード
 
 public:
+
+	//unitで初期化
+	SegmentTree(const size_t num){
+		for (length = 1; length < num; length *= 2);
+		node.resize(2 * length, Op.unitNode);
+	}
+
 	//vectorで初期化
-	SegmentTree(const vector<T> & vec, const T init, function<T(T,T)> funcNode, function<T(T,T)> funcMerge) : init(init), funcNode(funcNode), funcMerge(funcMerge) {
+	SegmentTree(const vector<typeNode> & vec){
 		for (length = 1; length < vec.size(); length *= 2);
-		node.resize(2 * length, init);
+		node.resize(2 * length, Op.unitNode);
 		for (int i = 0; i < vec.size(); ++i) node[i + length] = vec[i];
-		for (int i = length - 1; i >= 0; --i) node[i] = funcNode(node[(i<<1)+0],node[(i<<1)+1]);
+		for (int i = length - 1; i >= 0; --i) node[i] = Op.funcNode(node[(i<<1)+0],node[(i<<1)+1]);
 	}
  
-	//lengthだけで初期化
-	SegmentTree(const size_t num, const T init, function<T(T,T)> funcNode, function<T(T,T)> funcMerge) : init(init), funcNode(funcNode), funcMerge(funcMerge) {
+	//同じinitで初期化
+	SegmentTree(const size_t num, const typeNode init) {
 		for (length = 1; length < num; length *= 2);
 		node.resize(2 * length, init);
 	}
 	
 	//idx : 0-indexed
-	void update(size_t idx, const T var) {
+	void update(size_t idx, const typeNode var) {
 		if(idx < 0 || length <= idx) return;
 		idx += length;
-		node[idx] = funcMerge(node[idx],var);
-		while(idx >>= 1) node[idx] = funcNode(node[(idx<<1)+0],node[(idx<<1)+1]);
+		node[idx] = Op.funcMerge(node[idx],var);
+		while(idx >>= 1) node[idx] = Op.funcNode(node[(idx<<1)+0],node[(idx<<1)+1]);
 	}
 
 	//[l,r)
-	T get(int l, int r) {
-		if (l < 0 || length <= l || r < 0 || length < r) return init;
-		T vl = init, vr = init;
+	typeNode get(int l, int r) {
+		if (l < 0 || length <= l || r < 0 || length < r) return Op.unitNode;
+		typeNode vl = Op.unitNode, vr = Op.unitNode;
 		for(l += length, r += length; l < r; l >>=1, r >>=1) {
-			if(l&1) vl = funcNode(vl,node[l++]);
-			if(r&1) vr = funcNode(node[--r],vr);
+			if(l&1) vl = Op.funcNode(vl,node[l++]);
+			if(r&1) vr = Op.funcNode(node[--r],vr);
 		}
-		return funcNode(vl,vr);
+		return Op.funcNode(vl,vr);
 	}
  
 	void print(){
@@ -52,27 +59,25 @@ public:
 
 };
 
-//GCD
-//verify https://atcoder.jp/contests/abc125/tasks/abc125_c
+//一点加算 区間最大
+template<class typeNode> struct nodeSumRangeMax {
+	typeNode unitNode = 0;
+	typeNode funcNode(typeNode l,typeNode r){return max(l,r);}
+	typeNode funcMerge(typeNode l,typeNode r){return l+r;}
+};
 
-//SUM
-//verify https://atcoder.jp/contests/arc077/tasks/arc077_c
+//一点加算 区間総和
+template<class typeNode> struct nodeSumRangeSum {
+	typeNode unitNode = 0;
+	typeNode funcNode(typeNode l,typeNode r){return l+r;}
+	typeNode funcMerge(typeNode l,typeNode r){return l+r;}
+};
 
-//MIN
-//verify https://atcoder.jp/contests/bitflyer2018-final-open/tasks/bitflyer2018_final_c
+//一点更新 区間最小
+template<class typeNode> struct nodeUpdateRangeMin {
+	typeNode unitNode = (1LL<<31)-1;
+	typeNode funcNode(typeNode l,typeNode r){return min(l,r);}
+	typeNode funcMerge(typeNode l,typeNode r){return r;}
+};
 
-//MAX
-//verify https://atcoder.jp/contests/wupc2019/tasks/wupc2019_d
-
-	// //一点加算 + 区間総和
-	// function<ll(ll,ll)> funcNode =  [&](ll a,ll b){return a+b;};
-	// function<ll(ll,ll)> funcMerge = [&](ll a,ll b){return a+b;};
-
-	// //一点更新 + 区間GCD
-	// function<ll(ll,ll)> funcNode =  GCD;
-	// function<ll(ll,ll)> funcMerge = [&](ll a,ll b){return b;};
-
-	// //一点更新 + 区間最小
-	// function<ll(ll,ll)> funcNode =  [&](ll a,ll b){return min(a,b);};
-	// function<ll(ll,ll)> funcMerge = [&](ll a,ll b){return b;};
 
