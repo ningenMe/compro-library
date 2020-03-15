b8015ce8a3e98843230b7bea230276505743fc12
ningenme <amohkuru@gmail.com>
2020-03-16 00:46:10 +0900

    インデント調整

--- MinimumDirectedClosedCircuit.cpp
+++ MinimumDirectedClosedCircuit.cpp
@@ -10,12 +10,12 @@ public:
 	}
 	void makeEdge(int from,int to){
 		edge[from].push_back(to);
-        redge[to].push_back(from);
+		redge[to].push_back(from);
 	}
 	//rootを含む最小閉路の集合を返す O(N) 閉路がないときは空集合
 	vector<int> solve(int root,int inf = 123456789){
 		int mini = inf;
-        int last = -1;
+		int last = -1;
 		for(int i = 0; i < N; ++i) dist[i] = -1;
 		queue<int> q;
 		q.push(root);
@@ -24,28 +24,28 @@ public:
 			int curr = q.front();
 			q.pop();
 			for(int next:edge[curr]){
-                if(next == root && mini > dist[curr]+1) mini = dist[curr]+1,last = curr;
+				if(next == root && mini > dist[curr]+1) mini = dist[curr]+1,last = curr;
 				if(dist[next]==-1) {
 					dist[next] = dist[curr] + 1;
 					q.push(next);
 				}
 			}
 		}
-        vector<int> res;
-        if(last != -1){
-            res.push_back(last);
-            int curr = last;
-            while(curr != root){
-                for(int next:redge[curr]){
-                    if(dist[next]+1==dist[curr]) {
-                        res.push_back(next);
-                        curr = next;
-                        break;
-                    }
-                }
-            }
-            reverse(res.begin(),res.end());
-        }
+		vector<int> res;
+		if(last != -1){
+			res.push_back(last);
+			int curr = last;
+			while(curr != root){
+				for(int next:redge[curr]){
+					if(dist[next]+1==dist[curr]) {
+						res.push_back(next);
+						curr = next;
+						break;
+					}
+				}
+			}
+			reverse(res.begin(),res.end());
+		}
 		return res;
 	}
 };
