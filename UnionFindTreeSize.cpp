15dc5c68ceea020c049affeb78892394f1697ee8
kuru <amohkuru@gmail.com>
2020-04-06 02:04:45 +0900

    add HLDecomposition

--- UnionFindTreeSize.cpp
+++ UnionFindTreeSize.cpp
@@ -1,9 +1,14 @@
+
 //Union Find Tree
 class UnionFindTreeSize {
 public:
 	vector<int> parent;
+    vector<int> maxidx;
+    vector<int> minidx;
 
-    UnionFindTreeSize(int N = 1) : parent(N,-1){
+    UnionFindTreeSize(int N = 1) : parent(N,-1),maxidx(N),minidx(N){
+        iota(maxidx.begin(),maxidx.end(),0);
+        iota(minidx.begin(),minidx.end(),0);
 	}
  
 	int root(int n) {
@@ -13,6 +18,14 @@ public:
     bool same(int n, int m) {
 		return root(n) == root(m);
 	}
+
+    int mini(int n) {
+        return minidx[root(n)];
+    }
+
+    int maxi(int n) {
+        return maxidx[root(n)];
+    }
  
 	void unite(int n, int m) {
 		n = root(n);
@@ -21,12 +34,16 @@ public:
 		if(parent[n]>parent[m]) swap(n, m);
         parent[n] += parent[m];
         parent[m] = n;
+        maxidx[n] = max(maxidx[n],maxidx[m]);
+        minidx[n] = min(minidx[n],minidx[m]);
 	}
 
     int size(int n){
         return (-parent[root(n)]);
     }
+
+    void print() {
+        for(int i = 0; i < parent.size(); ++i) cout << root(i) << " ";
+        cout << endl;
+    }
 };
-
-
-//verify 
