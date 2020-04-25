5f752648ae3bc9a9e86f9fe92a1eb9b025f55418
kuru <amohkuru@gmail.com>
2020-04-26 05:31:03 +0900

    add test segtree rmq

---
+++ test/segment-tree/SegmentTree-rmq.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_A"
+
+#include <vector>
+#include <iostream>
+using namespace std;
+#include "../../segment-tree/SegmentTree.cpp"
+
+int main(void){
+	int N,Q; cin >> N >> Q;
+	SegmentTree<nodeMinPointUpdate<long long>> Seg(N);
+	while(Q--){
+		long long q,a,b;
+		cin >> q >> a >> b;
+		if(q) cout << Seg.get(a,b+1) << endl;
+		else Seg.update(a,b);
+	}
+	return 0;
+}
