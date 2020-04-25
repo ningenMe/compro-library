bcceacbbec5b73c0219c40154268768c71364d01
kuru <amohkuru@gmail.com>
2020-04-26 05:49:07 +0900

    modify

--- segment-tree/SegmentTree.cpp
+++ segment-tree/SegmentTree.cpp
@@ -100,7 +100,7 @@ template<class T> struct nodeGcdPointUpdate {
 //一点更新 区間最小
 template<class T> struct nodeMinPointUpdate {
 	using TypeNode = T;
-	inline static constexpr TypeNode unit_node = 1LL<<40;
+	inline static constexpr TypeNode unit_node = (1LL<<31)-1;
 	inline static constexpr TypeNode funcNode(TypeNode l,TypeNode r){return min(l,r);}
 	inline static constexpr TypeNode funcMerge(TypeNode l,TypeNode r){return r;}
 	inline static constexpr bool funcCheck(TypeNode nodeVal,TypeNode var){return var == nodeVal;}
