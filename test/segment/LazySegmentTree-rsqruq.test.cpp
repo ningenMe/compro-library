5721e7249a22617cfe0c068b0e4103cecdee676f
kuru <amohkuru@gmail.com>
2020-04-26 18:36:32 +0900

    modify

--- test/segment/LazySegmentTree-rsqruq.test.cpp
+++ test/segment/LazySegmentTree-rsqruq.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_H"
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_I"
 
 #include <vector>
 #include <iostream>
