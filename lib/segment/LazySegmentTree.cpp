8260f41ae2fa80eb74849ee3b650c48a387cb100
kuru <amohkuru@gmail.com>
2020-04-26 18:21:21 +0900

    add LazySegmentTree

---
+++ lib/segment/LazySegmentTree.cpp
@@ -0,0 +1,146 @@
+template<class Operator> class LazySegmentTree {
+	using TypeNode = typename Operator::TypeNode;          
+	using TypeLazy = typename Operator::TypeLazy;
+	size_t num;      
+	size_t length;                                   
+	size_t height;                                   
+	vector<TypeNode> node;                           
+	vector<TypeLazy> lazy;                           
+	vector<pair<size_t,size_t>> range;
+
+	void propagate(int k) {
+		if(lazy[k] == Operator::unit_lazy) return;
+		node[k] = Operator::func_merge(node[k],lazy[k],range[k].second-range[k].first);
+		if(k < length) lazy[2*k+0] = Operator::func_lazy(lazy[2*k+0],lazy[k]);
+		if(k < length) lazy[2*k+1] = Operator::func_lazy(lazy[2*k+1],lazy[k]);
+		lazy[k] = Operator::unit_lazy;
+	}
+public:
+
+	//unitで初期化
+	LazySegmentTree(const size_t num) : num(num) {
+		for (length = 1,height = 0; length < num; length *= 2, height++);
+		node.resize(2 * length, Operator::unit_node);
+		lazy.resize(2 * length, Operator::unit_lazy);
+		for (int i = 0; i < num; ++i) node[i + length] = Operator::unit_node;
+		for (int i = length - 1; i >= 0; --i) node[i] = Operator::func_node(node[(i<<1)+0],node[(i<<1)+1]);
+		range.resize(2 * length);
+		for (int i = 0; i < length; ++i) range[i+length] = make_pair(i,i+1);
+		for (int i = length - 1; i >= 0; --i) range[i] = make_pair(range[(i<<1)+0].first,range[(i<<1)+1].second);
+	}
+
+	// //同じinitで初期化
+	LazySegmentTree(const size_t num, const TypeNode init) : num(num) {
+		for (length = 1,height = 0; length < num; length *= 2, height++);
+		node.resize(2 * length, Operator::unit_node);
+		lazy.resize(2 * length, Operator::unit_lazy);
+		for (int i = 0; i < num; ++i) node[i + length] = init;
+		for (int i = length - 1; i >= 0; --i) node[i] = Operator::func_node(node[(i<<1)+0],node[(i<<1)+1]);
+		range.resize(2 * length);
+		for (int i = 0; i < length; ++i) range[i+length] = make_pair(i,i+1);
+		for (int i = length - 1; i >= 0; --i) range[i] = make_pair(range[(i<<1)+0].first,range[(i<<1)+1].second);
+	}
+
+	//vectorで初期化
+	LazySegmentTree(const vector<TypeNode>& vec) : num(vec.size()) {
+		for (length = 1,height = 0; length < vec.size(); length *= 2, height++);
+		node.resize(2 * length, Operator::unit_node);
+		lazy.resize(2 * length, Operator::unit_lazy);
+		for (int i = 0; i < vec.size(); ++i) node[i + length] = vec[i];
+		for (int i = length - 1; i >= 0; --i) node[i] = Operator::func_node(node[(i<<1)+0],node[(i<<1)+1]);
+		range.resize(2 * length);
+		for (int i = 0; i < length; ++i) range[i+length] = make_pair(i,i+1);
+		for (int i = length - 1; i >= 0; --i) range[i] = make_pair(range[(i<<1)+0].first,range[(i<<1)+1].second);
+	}
+
+	//update [a,b)
+	void update(int a, int b, TypeLazy x) {
+		int l = a + length, r = b + length - 1;
+		for (int i = height; 0 < i; --i) propagate(l >> i), propagate(r >> i);
+		for(r++; l < r; l >>=1, r >>=1) {
+			if(l&1) lazy[l] = Operator::func_lazy(lazy[l],x), propagate(l),l++;
+			if(r&1) --r,lazy[r] = Operator::func_lazy(lazy[r],x), propagate(r);
+		}
+		l = a + length, r = b + length - 1;
+		while ((l>>=1),(r>>=1),l) {
+			if(lazy[l] == Operator::unit_lazy) node[l] = Operator::func_node(Operator::func_merge(node[(l<<1)+0],lazy[(l<<1)+0],range[(l<<1)+0].second-range[(l<<1)+0].first),Operator::func_merge(node[(l<<1)+1],lazy[(l<<1)+1],range[(l<<1)+1].second-range[(l<<1)+1].first));
+			if(lazy[r] == Operator::unit_lazy) node[r] = Operator::func_node(Operator::func_merge(node[(r<<1)+0],lazy[(r<<1)+0],range[(r<<1)+0].second-range[(r<<1)+0].first),Operator::func_merge(node[(r<<1)+1],lazy[(r<<1)+1],range[(r<<1)+1].second-range[(r<<1)+1].first));
+		}
+	}
+
+	//get [a,b)
+	TypeNode get(int a, int b) {
+		int l = a + length, r = b + length - 1;
+		for (int i = height; 0 < i; --i) propagate(l >> i), propagate(r >> i);
+		TypeNode vl = Operator::unit_node, vr = Operator::unit_node;
+		for(r++; l < r; l >>=1, r >>=1) {
+			if(l&1) vl = Operator::func_node(vl,Operator::func_merge(node[l],lazy[l],range[l].second-range[l].first)),l++;
+			if(r&1) r--,vr = Operator::func_node(Operator::func_merge(node[r],lazy[r],range[r].second-range[r].first),vr);
+		}
+		return Operator::func_node(vl,vr);
+	}
+
+	//return [0,length]
+	int prefix_binary_search(TypeNode var) {
+		int l = length, r = 2*length - 1;
+		for (int i = height; 0 < i; --i) propagate(l >> i), propagate(r >> i);
+		if(!Operator::func_check(node[1],var)) return num;
+		TypeNode ret = Operator::unit_node;
+		size_t idx = 2;
+		for(; idx < 2*length; idx<<=1){
+			if(!Operator::func_check(Operator::func_node(ret,Operator::func_merge(node[idx],lazy[idx],range[idx].second-range[idx].first)),var)) {
+				ret = Operator::func_node(ret,Operator::func_merge(node[idx],lazy[idx],range[idx].second-range[idx].first));
+				idx++;
+			}
+		}
+		return min((idx>>1) - length,num);
+	}
+
+	//range[l,r) return [l,r]
+	int binary_search(size_t l, size_t r, TypeNode var) {
+		if (l < 0 || length <= l || r < 0 || length < r) return -1;
+		for (int i = height; 0 < i; --i) propagate((l+length) >> i), propagate((r+length-1) >> i);
+		TypeNode ret = Operator::unit_node;
+		size_t off = l;
+		for(size_t idx = l+length; idx < 2*length && off < r; ){
+			if(range[idx].second<=r && !Operator::func_check(Operator::func_node(ret,Operator::func_merge(node[idx],lazy[idx],range[idx].second-range[idx].first)),var)) {
+				ret = Operator::func_node(ret,Operator::func_merge(node[idx],lazy[idx],range[idx].second-range[idx].first));
+				off = range[idx++].second;
+				if(!(idx&1)) idx >>= 1;			
+			}
+			else{
+				idx <<=1;
+			}
+		}
+		return off;
+	}
+
+	void print(){
+		// cout << "node" << endl;
+		// for(int i = 1,j = 1; i < 2*length; ++i) {
+		// 	cout << node[i] << " ";
+		// 	if(i==((1<<j)-1) && ++j) cout << endl;
+		// }
+		// cout << "lazy" << endl;
+		// for(int i = 1,j = 1; i < 2*length; ++i) {
+		// 	cout << lazy[i] << " ";
+		// 	if(i==((1<<j)-1) && ++j) cout << endl;
+		// }
+		cout << "vector" << endl;
+		cout << "{ " << get(0,1);
+		for(int i = 1; i < length; ++i) cout << ", " << get(i,i+1);
+		cout << " }" << endl;
+	}
+};
+
+//node:最小　lazy:加算
+template<class T, class U> struct NodeMinRangeAdd {
+	using TypeNode = T;
+	using TypeLazy = U;
+	inline static constexpr TypeNode unit_node = 1234567890;
+	inline static constexpr TypeLazy unit_lazy = 0;
+	inline static constexpr TypeNode func_node(TypeNode l,TypeNode r){return min(l,r);}
+	inline static constexpr TypeLazy func_lazy(TypeLazy l,TypeLazy r){return l+r;}
+	inline static constexpr TypeNode func_merge(TypeNode l,TypeLazy r,int len){return l+r;}
+	// LazySegmentTree<NodeMinRangeAdd<ll,ll>> Seg(N,0);
+};
