8f416e6638181a6c59b4fdc01518773f8a92bf3b
kuru <amohkuru@gmail.com>
2020-04-28 06:42:13 +0900

    add CombinationMod

---
+++ lib/math/CombinationMod.cpp
@@ -0,0 +1,26 @@
+//Combination Mod
+template<long long mod> class CombinationMod {
+	vector<long long> fac,finv,inv;
+public:
+	CombinationMod(int N) : fac(N + 1), finv(N + 1), inv(N + 1) {
+		fac[0] = fac[1] = finv[0] = finv[1] = inv[1] = 1;
+		for (int i = 2; i <= N; ++i) {
+			fac[i] = fac[i - 1] * i % mod;
+			inv[i] = mod - inv[mod%i] * (mod / i) % mod;
+			finv[i] = finv[i - 1] * inv[i] % mod;
+		}
+	}
+	inline long long binom(int n, int k) {
+		return ((n < 0 || k < 0 || n < k) ? 0 : fac[n] * (finv[k] * finv[n - k] % mod) % mod);
+	}
+    inline long long factorial(int n) {
+        return fac[n];
+    }
+    inline static constexpr vector<long long> pow2(int n) {
+        vector<long long> pow2(n+1,1);
+        for (int i = 2; i <= n; ++i) pow2[i] = (pow2[i - 1] * 2) % mod;
+        return pow2;
+    }
+};
+
+//verify https://atcoder.jp/contests/abc021/tasks/abc021_d
