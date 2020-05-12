13640e2b5296842922d3bd67993bade6fffaee2a
ningenMe <amohkuru@gmail.com>
2020-05-13 04:05:14 +0900

    add argument sort

---
+++ lib/geometory/Argument.cpp
@@ -0,0 +1,15 @@
+/*
+ * @title Argument
+ */
+class Argument {
+public:
+    template<class T> inline static vector<int> sort_by_atan2l(vector<pair<T,T>> points){
+        int N = points.size();
+        vector<long double> arg(N);
+        for(int i = 0; i < N; ++i) arg[i] = atan2l(points[i].second,points[i].first);
+        vector<int> res(N);
+        iota(res.begin(),res.end(),0);
+        sort(res.begin(),res.end(),[&](int l,int r){return arg[l] < arg[r];});
+        return res;
+    }
+};
