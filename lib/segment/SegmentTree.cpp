b9cbe8a40921ac91bcf8498c1c3a9cbe738d90c9
ningenMe <amohkuru@gmail.com>
2020-05-13 03:18:45 +0900

    add test

--- lib/segment/SegmentTree.cpp
+++ lib/segment/SegmentTree.cpp
@@ -100,11 +100,20 @@ template<class T> struct NodeMinPointUpdate {
 	inline static constexpr bool func_check(TypeNode nodeVal,TypeNode var){return var == nodeVal;}
 };
 
-//一点更新 区間最小
+//一点加算 区間最大
 template<class T> struct NodeSumPointAdd {
 	using TypeNode = T;
 	inline static constexpr TypeNode unit_node = 0;
 	inline static constexpr TypeNode func_node(TypeNode l,TypeNode r){return l+r;}
 	inline static constexpr TypeNode func_merge(TypeNode l,TypeNode r){return l+r;}
+	inline static constexpr bool func_check(TypeNode nodeVal,TypeNode var){return var == nodeVal;}
+};
+
+//一次関数
+template<class T> struct NodeCompositePointUpdate {
+	using TypeNode = T;
+	inline static constexpr TypeNode unit_node = make_pair(1,0);
+	inline static constexpr TypeNode func_node(TypeNode l,TypeNode r){return {r.first*l.first,r.first*l.second+r.second};}
+	inline static constexpr TypeNode func_merge(TypeNode l,TypeNode r){return r;}
 	inline static constexpr bool func_check(TypeNode nodeVal,TypeNode var){return var == nodeVal;}
 };
---
+++ test/segment/SegmentTree-rcq.test.cpp
@@ -0,0 +1,27 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_set_range_composite"
+
+#include <vector>
+#include <iostream>
+using namespace std;
+#include "../../lib/segment/SegmentTree.cpp"
+#include "../../lib/util/ModInt.cpp"
+
+using modint = ModInt<998244353>;
+int main(){
+	cin.tie(0);ios::sync_with_stdio(false);
+    int N,Q; cin >> N >> Q;
+    vector<pair<modint,modint>> A(N);
+    for(int i = 0; i < N; ++i) {
+        cin >> A[i].first >> A[i].second;
+    }
+    SegmentTree<NodeCompositePointUpdate<pair<modint,modint>>> seg(A);
+    while(Q--){
+        long long q,l,r,x; cin >> q >> l >> r >> x;
+        if(q) {
+			auto p = seg.get(l,r);
+			cout << p.first*x+p.second << endl;
+		}
+        else seg.update(l,{r,x});
+    }
+}
+
