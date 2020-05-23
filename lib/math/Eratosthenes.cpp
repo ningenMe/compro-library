057b3ed82307ee02a91cf8edc48a825b1eb069ed
ningenMe <amohkuru@gmail.com>
2020-05-24 02:56:21 +0900

    add Eratosthenes

--- lib/math/Eratosthenes.cpp
+++ lib/math/Eratosthenes.cpp
@@ -1,9 +1,10 @@
-//Eratosthenes O(N)
-vector<int> Eratosthenes(int N) {
+/*
+ * @title Eratosthenes
+ * @docs md/math/Eratosthenes.md
+ */
+inline static vector<int> Eratosthenes(int N) {
     vector<int> res(N + 1, 1);
     res[0] = res[1%(N+1)] = 0;
     for (int i = 1; i*i <= N; i++) if (res[i]) for (int j = 2 * i; j <= N; j += i) res[j] = 0;
     return res;
 }
-
-//verify https://atcoder.jp/contests/abc084/tasks/abc084_d
