aab462f9b10b565c47a05acdd392851326a37378
ningenMe <amohkuru@gmail.com>
2020-05-30 13:47:40 +0900

    modify

--- test/math/Garner.test.cpp
+++ test/math/Garner.test.cpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <iostream>
+#include <numeric>
 using namespace std;
 #include "../../lib/math/Garner.cpp"
 constexpr long long MOD = 1000'000'007;
@@ -9,14 +10,9 @@ constexpr long long MOD = 1000'000'007;
 int main(void){
     int N; cin >> N;
 	vector<long long> b(N), m(N);
-	int flg = 1;
 	for(int i = 0; i < N; ++i){
 		cin >> b[i] >> m[i];
-		if(b[i]) flg = 0;
 	}
-	auto [r,lcm]=Garner<MOD>::garner(b,m);
-	long long ans = r;
-	if(flg) ans = lcm;
-	cout << ans << endl;
+	cout << Garner<MOD>::garner(b,m) << endl;
 	return 0;
 }
