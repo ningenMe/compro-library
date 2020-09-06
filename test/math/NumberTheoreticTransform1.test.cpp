f7c713c8f6e07bff094d8842bbd7b518690aae8f
ningenMe <amohkuru@gmail.com>
2020-09-07 02:54:26 +0900

    [modify]

--- test/math/NumberTheoreticTransform1.test.cpp
+++ test/math/NumberTheoreticTransform1.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/convolution_mod_1000000007"
+#define PROBLEM "https://judge.yosupo.jp/problem/convolution_mod"
 
 #include <vector>
 #include <iostream>
