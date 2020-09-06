0a4709b7d8933860c3342372cf1358871122f058
ningenMe <amohkuru@gmail.com>
2020-09-07 01:58:37 +0900

    [modify]

--- test/math/NumberTheoreticTransformArbitraryMod.test.cpp
+++ test/math/NumberTheoreticTransformArbitraryMod.test.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <numeric>
 using namespace std;
+#include "../../lib/util/ModInt.cpp"
 #include "../../lib/math/NumberTheoreticTransform.cpp"
 constexpr long long MOD = 1000'000'007;
 
@@ -14,8 +15,8 @@ int main(void){
     for(int i = 0; i < N+1; ++i) cin >> B[i];
     NumberTheoreticTransform<MOD> ntt;
     auto C = ntt.convolution_arbitrarymod(A,B);
-    long long ans = 0;
-    for(int i = 0; i <= N; ++i) (ans += C[i]) %= MOD;
+    ModInt<MOD> ans = 0;
+    for(int i = 0; i <= N; ++i) ans += C[i];
     cout << ans << endl;
 	return 0;
 }
