6c52d96eed418103e4465a21ad39b2241faa4a47
ningenMe <amohkuru@gmail.com>
2020-09-26 08:49:44 +0900

    [modify] url

--- test/math/FormalPowerSeries-log.test.cpp
+++ test/math/FormalPowerSeries-log.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/inv_of_formal_power_series"
+#define PROBLEM "https://judge.yosupo.jp/problem/log_of_formal_power_series"
 
 #include <vector>
 #include <iostream>
