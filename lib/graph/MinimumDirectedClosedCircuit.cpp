fb736d0eb3cc53e25534cdcff2ef11141bb22c54
ningenMe <amohkuru@gmail.com>
2020-12-02 19:42:18 +0900

    [modify] mdcc

--- lib/graph/MinimumDirectedClosedCircuit.cpp
+++ lib/graph/MinimumDirectedClosedCircuit.cpp
@@ -9,38 +9,11 @@ template<class T> class MinimumDirectedClosedCircuit {
     vector<T> dist;
     vector<int> parent;
     size_t N;
-    bool is_same_weighted;
     T inf;
     int last,root;
 private:
-    T solve_same_weighted() {
-        T mini = inf;
-        last = -1;
-        queue<int> q;
-        q.push(root);
-        dist[root] = 0;
-        while (q.size()) {
-            size_t curr = q.front(); q.pop();
-            for(auto& edge:graph.edges[curr]){
-                size_t next = edge.first;
-                T w  = edge.second;
-                
-                //根に返って来てるなら閉路
-                if(next == root && mini > dist[curr]+w) {
-                    mini = dist[curr]+w;
-                    last = curr;
-                }
-                //そうじゃないなら探索木を広げる
-                else if(dist[next]==-1) {
-                    dist[next]   = dist[curr] + w;
-                    parent[next] = curr;
-                    q.push(next);
-                }
-            }
-        }
-        return mini;
-    }
-    T solve_diff_weighted() {
+
+    T solve_impl() {
         T mini = inf;
         last = -1;
         RadixHeap<int> q(0);
@@ -51,19 +24,17 @@ private:
             size_t curr = top.second;
             for(auto& edge:graph.edges[curr]){
                 size_t next = edge.first;
-                T w  = edge.second;
-                
-                //根に返って来てるなら閉路
-                if(next == root && mini > dist[curr]+w) {
-                    mini = dist[curr]+w;
-                    last = curr;
-                }
-                //そうじゃないなら探索木を広げる
-                else if(dist[next]==-1) {
+                T w  = edge.second;                
+                if(dist[next] > dist[curr]+w) {
                     dist[next]   = dist[curr] + w;
                     parent[next] = curr;
                     q.push({dist[next],next});
                 }
+                //根に返って来てるなら閉路候補
+                if(next == root && mini > dist[curr]+w) {
+                    mini = dist[curr]+w;
+                    last = curr;
+                }                
             }
         }
         return mini;
@@ -71,28 +42,21 @@ private:
 public:
     MinimumDirectedClosedCircuit(Graph<T>& graph, T inf)
      : graph(graph),N(graph.size()),dist(graph.size()),parent(graph.size()),inf(inf) {
-        assert(!graph.edges.empty());
-        //重みが一律かどうか判定 面倒だからここはlogつき
-        set<T> st;
-        for(int i=0;i<N;++i) for(auto& edge:graph.edges[i]) st.insert(edge.second);        
-        is_same_weighted = (st.size() == 1);
     }
     //rootを含む最小閉路の集合を返す O(NlogN) 閉路がないときは空集合
-    inline T solve(size_t rt, int restore = 0){
+    inline T solve(size_t rt){
         root = rt;
         //初期化
-        for(int i = 0; i < N; ++i) dist[i] = parent[i] = -1;
+        for(int i = 0; i < N; ++i) dist[i] = inf, parent[i] = -1;
         //最小閉路の大きさを決める
-        T mini;
-        if(is_same_weighted) mini=solve_same_weighted(); //重み一律
-        else mini=solve_diff_weighted(); //重みがバラバラ
+        T mini = solve_impl();
         return mini;
     }
     vector<int> restore() {
         vector<int> res;
         if(last == -1) return res;
-        res.push_back(last);
         int curr = last;
+        res.push_back(curr);
         while(curr != root) res.push_back(curr = parent[curr]);
         reverse(res.begin(),res.end());
         return res;
