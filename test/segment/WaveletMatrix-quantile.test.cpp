1c72119c2c3b4de25bfe5cd9ba53809afbe0b82e
ningenMe <amohkuru@gmail.com>
2021-01-04 03:34:23 +0900

    [add] wavelet matrix

---
+++ test/segment/WaveletMatrix-quantile.test.cpp
@@ -0,0 +1,34 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/range_kth_smallest
+
+#include <vector>
+#include <iostream>
+#include <cassert>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+using int128  = __int128_t;
+using int64   = long long;
+using int32   = int;
+using uint128 = __uint128_t;
+using uint64  = unsigned long long;
+using uint32  = unsigned int;
+
+#include "../../lib/segment/BitVector.cpp"
+#include "../../lib/segment/WaveletMatrix.cpp"
+
+int main() {
+    cin.tie(0);ios::sync_with_stdio(false);
+
+	int N, Q;
+	cin >> N >> Q;
+	vector<uint32> A(N);
+    for(int i=0; i<N; ++i) cin >> A[i];
+	WaveletMatrix<uint32> wm(A);
+    while(Q--) {
+        int l,r; uint32 k;
+		cin >> l >> r >> k;
+		cout << wm.quantile(l, r, k) << "\n";
+	}
+
+    return 0;
+}
