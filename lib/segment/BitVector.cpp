4f08b76fcf9b93c7d014d9da60019bb37163d3e1
ningenMe <amohkuru@gmail.com>
2021-01-04 03:42:26 +0900

    [modify]

--- lib/segment/BitVector.cpp
+++ lib/segment/BitVector.cpp
@@ -8,33 +8,33 @@ class BitVector{
     inline static constexpr uint32 popcount(uint32 x) {return __builtin_popcount(x);}
     inline static constexpr uint32 popcount(uint64 x) {return __builtin_popcountll(x);}
 
-	vector<uint32> vec, acc;
+    vector<uint32> vec, acc;
     bool is_builded = false;
 public:
-	BitVector(uint32 N) {
+    BitVector(uint32 N) {
         uint32 tmp = (N + BIT_BLOCK_NUM-1) / BIT_BLOCK_NUM;
-		vec.assign(tmp, 0);
-		acc.assign(tmp, 0);
-	}
-	void build() {
-		for (size_t i = 0,sum = 0; i < acc.size(); ++i) acc[i] = (sum += popcount(vec[i]) );
+        vec.assign(tmp, 0);
+        acc.assign(tmp, 0);
+    }
+    void build() {
+        for (size_t i = 0,sum = 0; i < acc.size(); ++i) acc[i] = (sum += popcount(vec[i]) );
         is_builded = true;
-	}
-	//[0,r) count of bit
-	uint32 rank(uint32 r, const bool bit) const {
+    }
+    //[0,r) count of bit
+    uint32 rank(uint32 r, const bool bit) const {
         assert(is_builded);
-		uint32 bit_r = r >> BIT_BLOCK_SIZE;
-		uint32 sum = (bit_r ? acc[bit_r - 1] : 0) + popcount(vec[bit_r] & ((1U << (r & (BIT_BLOCK_NUM-1))) - 1));
-		return bit ? sum : r - sum;
-	}
+        uint32 bit_r = r >> BIT_BLOCK_SIZE;
+        uint32 sum = (bit_r ? acc[bit_r - 1] : 0) + popcount(vec[bit_r] & ((1U << (r & (BIT_BLOCK_NUM-1))) - 1));
+        return bit ? sum : r - sum;
+    }
     //[l,l+1) = bit
-	void update(uint32 l, const bool bit) {
-		uint32 bit_l = l >> BIT_BLOCK_SIZE;
-		if (bit) vec[bit_l] |=   1U << (l & (BIT_BLOCK_NUM-1));
-		else     vec[bit_l] &= ~(1U << (l & (BIT_BLOCK_NUM-1)));
-	}
+    void update(uint32 l, const bool bit) {
+        uint32 bit_l = l >> BIT_BLOCK_SIZE;
+        if (bit) vec[bit_l] |=   1U << (l & (BIT_BLOCK_NUM-1));
+        else     vec[bit_l] &= ~(1U << (l & (BIT_BLOCK_NUM-1)));
+    }
     //[l,l+1)
-	bool operator[](uint32 l) const { 
+    bool operator[](uint32 l) const { 
         assert(is_builded);
         return ((vec[l >> BIT_BLOCK_SIZE] >> (l & (BIT_BLOCK_NUM-1))) & 1); 
     }
--- lib/segment/WaveletMatrix.cpp
+++ lib/segment/WaveletMatrix.cpp
@@ -3,10 +3,10 @@
  * @docs md/segment/WaveletMatrix.md
  */
 template<class T> class WaveletMatrix{
-	size_t length;
+    size_t length;
     size_t depth;
-	vector<BitVector> multi_bit_vector;
-	vector<uint32> sum_bit_off;
+    vector<BitVector> multi_bit_vector;
+    vector<uint32> sum_bit_off;
     vector<T> vec;
     vector<uint32> zarts(const vector<T>& ar) {
         vector<uint32> ord(ar.size()),compressed(ar.size());
@@ -24,7 +24,7 @@ template<class T> class WaveletMatrix{
         return compressed;
     }
 public:
-	WaveletMatrix(const vector<T> &arg_vec): vec(arg_vec) {
+    WaveletMatrix(const vector<T> &arg_vec): vec(arg_vec) {
         length = vec.size();
         auto compressed = zarts(vec);
         sort(vec.begin(),vec.end());
@@ -32,60 +32,60 @@ public:
         uint32 maxi = *max_element(compressed.begin(),compressed.end()) + 1;
         for(depth=0; (1<<depth) <= maxi; ++depth );
 
-		multi_bit_vector.assign(depth, BitVector(length));
-		sum_bit_off.assign(depth, 0UL);
-		vector<uint32> prev = compressed, next = prev;
-		for(uint32 j = 0; j < depth; ++j,swap(prev,next)) {
-			uint32 bit = 1UL << (depth - j - 1);
-			for(uint32 i = 0; i < length; ++i) sum_bit_off[j] += !(prev[i] & bit);
+        multi_bit_vector.assign(depth, BitVector(length));
+        sum_bit_off.assign(depth, 0UL);
+        vector<uint32> prev = compressed, next = prev;
+        for(uint32 j = 0; j < depth; ++j,swap(prev,next)) {
+            uint32 bit = 1UL << (depth - j - 1);
+            for(uint32 i = 0; i < length; ++i) sum_bit_off[j] += !(prev[i] & bit);
 
-			uint32 idx_bit_off = 0, idx_bit_on = sum_bit_off[j];
-			for(uint32 i = 0; i < length; ++i) {
-				if (prev[i] & bit) multi_bit_vector[j].update(i,1), next[idx_bit_on++] = prev[i];
-				else next[idx_bit_off++] = prev[i];
+            uint32 idx_bit_off = 0, idx_bit_on = sum_bit_off[j];
+            for(uint32 i = 0; i < length; ++i) {
+                if (prev[i] & bit) multi_bit_vector[j].update(i,1), next[idx_bit_on++] = prev[i];
+                else next[idx_bit_off++] = prev[i];
             }
-			multi_bit_vector[j].build();
-		}
-	}
+            multi_bit_vector[j].build();
+        }
+    }
     //[l,l+1) element
-	T operator[](uint32 l) const {
-		uint32 val = 0;
-		for (uint32 j = 0; j < depth; ++j) {
-			const bool bit = multi_bit_vector[j][l];
-			val = ((val << 1) | bit);
-			l = multi_bit_vector[j].rank(l, bit);
-			if(bit) l += sum_bit_off[j];
-		}
-		return vec[val];
-	}
+    T operator[](uint32 l) const {
+        uint32 val = 0;
+        for (uint32 j = 0; j < depth; ++j) {
+            const bool bit = multi_bit_vector[j][l];
+            val = ((val << 1) | bit);
+            l = multi_bit_vector[j].rank(l, bit);
+            if(bit) l += sum_bit_off[j];
+        }
+        return vec[val];
+    }
 
     //[l,r) range count of more and less than c (c'<c, c'=c, c<c')
-	// tuple<uint32, uint32, uint32> rank_all(T c, uint32 l, uint32 r) const
-	// {
-	// 	if (c > vec.back()) return make_tuple(r - l, 0U, 0U);
-	// 	array<uint32,2> more_and_less = {0,0};
-	// 	for (uint32 j = 0; j < depth; ++j) {
-	// 		const bool bit = (c >> (depth - j - 1)) & 1;
-	// 		more_and_less[bit] += r - l;
-	// 		l = multi_bit_vector[j].rank(l, bit);
-	// 		r = multi_bit_vector[j].rank(r, bit);
-	// 		if (bit) l += sum_bit_off[j], r += sum_bit_off[j];
-	// 		more_and_less[bit] -= r - l;
-	// 	}
-	// 	return make_tuple(more_and_less[1], r - l, more_and_less[0]);
-	// }
+    // tuple<uint32, uint32, uint32> rank_all(T c, uint32 l, uint32 r) const
+    // {
+    // 	if (c > vec.back()) return make_tuple(r - l, 0U, 0U);
+    // 	array<uint32,2> more_and_less = {0,0};
+    // 	for (uint32 j = 0; j < depth; ++j) {
+    // 		const bool bit = (c >> (depth - j - 1)) & 1;
+    // 		more_and_less[bit] += r - l;
+    // 		l = multi_bit_vector[j].rank(l, bit);
+    // 		r = multi_bit_vector[j].rank(r, bit);
+    // 		if (bit) l += sum_bit_off[j], r += sum_bit_off[j];
+    // 		more_and_less[bit] -= r - l;
+    // 	}
+    // 	return make_tuple(more_and_less[1], r - l, more_and_less[0]);
+    // }
 
     //[l,r) range k (0-indexed) th smallest number
-	T quantile(uint32 l, uint32 r, uint32 k) const {
-		uint32 val = 0;
-		for (uint32 j = 0; j < depth; ++j) {
-			uint32 cnt_bit_off = multi_bit_vector[j].rank(r, 0) - multi_bit_vector[j].rank(l, 0);
-			const bool bit = (k >= cnt_bit_off);
-			val = ((val << 1) | bit);
-			l = multi_bit_vector[j].rank(l, bit);
-			r = multi_bit_vector[j].rank(r, bit);
-			if (bit) l += sum_bit_off[j], r += sum_bit_off[j], k -= cnt_bit_off;
-		}
-		return vec[val];
-	}
+    T quantile(uint32 l, uint32 r, uint32 k) const {
+        uint32 val = 0;
+        for (uint32 j = 0; j < depth; ++j) {
+            uint32 cnt_bit_off = multi_bit_vector[j].rank(r, 0) - multi_bit_vector[j].rank(l, 0);
+            const bool bit = (k >= cnt_bit_off);
+            val = ((val << 1) | bit);
+            l = multi_bit_vector[j].rank(l, bit);
+            r = multi_bit_vector[j].rank(r, bit);
+            if (bit) l += sum_bit_off[j], r += sum_bit_off[j], k -= cnt_bit_off;
+        }
+        return vec[val];
+    }
 };
