2f653f29d1e45dd0f34aeb77df13d126bc754f29
ningenMe <amohkuru@gmail.com>
2021-04-08 01:38:12 +0900

    [modify] test

--- test/math/NumberTheoreticalTransform-conv-998244353-2.test.cpp
+++ test/math/NumberTheoreticalTransform-conv-998244353-2.test.cpp
@@ -7,17 +7,16 @@
 #include <array>
 
 using namespace std;
-#include "../../lib/util/RuntimeModInt.cpp"
 #include "../../lib/util/ModInt.cpp"
-#include "../../lib/math/FormalPowerSeries.cpp"
+#include "../../lib/math/NumberTheoreticalTransform.cpp"
 constexpr long long MOD = 998244353;
-using fps = FormalPowerSeries<ModInt<MOD>>;
+using modint = ModInt<MOD>;
 
-vector<fps> v;
-fps rec(int l, int r) {
+vector<vector<modint>> v;
+vector<modint> rec(int l, int r) {
     if(r-l==1) return v[l];
-    if(r-l==2) return v[l]*v[l+1];
-    return rec(l,(l+r)/2)*rec((l+r)/2,r);
+    if(r-l==2) return NumberTheoreticalTransform<modint>::convolution(v[l],v[l+1]);
+    return NumberTheoreticalTransform<modint>::convolution(rec(l,(l+r)/2),rec((l+r)/2,r));
 }
 
 int main() {
