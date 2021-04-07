6d6c8ec7d9cf6027fd2ba8600dab3868d555319f
ningenMe <amohkuru@gmail.com>
2021-04-08 03:16:53 +0900

    [add] inv

--- lib/math/FormalPowerSeries.cpp
+++ lib/math/FormalPowerSeries.cpp
@@ -3,72 +3,52 @@
  * @docs md/math/FormalPowerSeries.md
  */
 template<class T> struct FormalPowerSeries : public vector<T> {
-    inline static constexpr int prime1 =1004535809;
-    inline static constexpr int prime2 =998244353;
-    inline static constexpr int prime3 =985661441;
-    inline static constexpr int inv21  =332747959; // ModInt<mod2>(mod1).inv().x;
-    inline static constexpr int inv31  =766625513; // ModInt<mod3>(mod1).inv().x;
-    inline static constexpr int inv32  =657107549; // ModInt<mod3>(mod2).inv().x;
-    inline static constexpr long long prime12=(1002772198720536577LL);
-    inline static constexpr array<int,26> pow2 = {1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576,2097152,4194304,8388608,16777216,33554432};
     using vector<T>::vector;
     using Mint  = T;
-    using Mint1 = ModInt<prime1>;
-    using Mint2 = ModInt<prime2>;
-    using Mint3 = ModInt<prime3>;
     using Fps   = FormalPowerSeries<T>;
+public:
+    inline static Fps add(const Fps& lhs,const Fps& rhs) {
+        size_t n = lhs.size(), m = rhs.size();
+        Fps res(max(n,m),0);
+        for(int i=0;i<n;++i) res[i] += lhs[i];
+        for(int i=0;i<m;++i) res[i] += rhs[i];
+        return res;
+    }
+    inline static Fps sub(const Fps& lhs,const Fps& rhs) {
+        size_t n = lhs.size(), m = rhs.size();
+        Fps res(max(n,m),0);
+        for(int i=0;i<n;++i) res[i] += lhs[i];
+        for(int i=0;i<m;++i) res[i] -= rhs[i];
+        return res;
+    }
+    inline static Fps mul(const Fps& lhs, const Fps& rhs) {
+        return NumberTheoreticalTransform<Mint>::convolution(lhs,rhs);
+    }
+    Fps inv(size_t n) const {
+        Fps ret({Mint(1)/(*this)[0]});
+        for(size_t i=2,m=(n<<1);i < m; i<<=1) {
+            Fps h = mul(mul(ret,ret),(this->prefix(i)));
+            ret.resize(i);
+            for(int j=i>>1;j<i;++j) ret[j] -= h[j];
+        }
+        return ret.prefix(n);
+    }
+    Fps inv(void) const {return inv(this->size());}
+    // Fps pow_impl(long long k,size_t n) const {
+    //     Fps ret(n,0);
+    //     for(size_t i=0; i < min(n,this->size()) && i*k < n; ++i) {
+    //         if((*this)[i].x == 0) continue;
+    //         Mint t0=(*this)[i], t0_inv=t0.inv();
+    //         Fps tmp(n-i);for(int j=i;j<min(n,this->size()); ++j) tmp[j-i]=(*this)[j]*t0_inv;
+    //         tmp = (tmp.log(n)*k).exp(n)*(t0.pow(k));
+    //         for(int j=0;j+i*k<n;++j) ret[j+i*k] = tmp[j];
+    //         break;
+    //     }
+    //     return ret;
+    // }
     Fps even(void) const {Fps ret;for(int i = 0; i < this->size(); i+=2) ret.push_back((*this)[i]);return ret;}
     Fps odd(void)  const {Fps ret;for(int i = 1; i < this->size(); i+=2) ret.push_back((*this)[i]);return ret;}
     Fps minus_x(void) const {Fps ret(this->size());for(int i = 0; i < ret.size(); ++i) ret[i] = (*this)[i]*(i&1?-1:1);return ret;}
-    inline Mint garner(const Mint1& b1,const Mint2& b2,const Mint3& b3) {Mint2 t2 = (b2-b1.x)*inv21;Mint3 t3 = ((b3-b1.x)*inv31-t2.x)*inv32;return Mint(Mint(prime12)*t3.x+b1.x+prime1*t2.x);}
-    template<int prime> inline void ntt(vector<ModInt<prime>>& f) {
-        const int N = f.size(), M = N>>1;
-        const int log2N = __builtin_ctz(N);
-        ModInt<prime> h(3);
-        vector<ModInt<prime>> g(N),base(log2N);
-        for(int i=0;i<log2N;++i) base[i] = h.pow((prime - 1)/pow2[i+1]);
-        for(int n=0;n<log2N;++n) {
-            const int& p = pow2[log2N-n-1];
-            ModInt<prime> w = 1;
-            for (int i=0,k=0;i<M;i+=p,k=i<<1,w*=base[n]) {
-                for(int j=0;j<p;++j) {
-                    ModInt<prime> l = f[k|j],r = w*f[k|j|p];
-                    g[i|j]   = l + r;
-                    g[i|j|M] = l - r;
-                }
-            }
-            swap(f,g);
-        }
-    }
-    template<int prime> inline vector<ModInt<prime>> convolution_friendrymod(const vector<Mint>& a,const vector<Mint>& b){
-        if (min(a.size(), b.size()) <= 60) {
-            vector<ModInt<prime>> f(a.size() + b.size() - 1);
-            for (int i = 0; i < a.size(); i++) for (int j = 0; j < b.size(); j++) f[i+j]+=a[i].x*b[j].x;
-            return f;
-        }
-        int N,M=a.size()+b.size()-1; for(N=1;N<M;N*=2);
-        ModInt<prime> inverse(N); inverse = inverse.inv();
-        vector<ModInt<prime>> g(N,0),h(N,0);
-        for(int i=0;i<a.size();++i) g[i]=a[i].x;
-        for(int i=0;i<b.size();++i) h[i]=b[i].x;
-        ntt<prime>(g); ntt<prime>(h);
-        for(int i = 0; i < N; ++i) g[i] *= h[i]*inverse;
-        reverse(g.begin()+1,g.end());
-        ntt<prime>(g);
-        return g;
-    }
-    inline vector<Mint> convolution_arbitrarymod(const vector<Mint>& g,const vector<Mint>& h){
-        auto f1 = convolution_friendrymod<prime1>(g, h);
-        auto f2 = convolution_friendrymod<prime2>(g, h);
-        auto f3 = convolution_friendrymod<prime3>(g, h);
-        vector<Mint> f(f1.size());
-        for(int i=0; i<f1.size(); ++i) f[i] = garner(f1[i],f2[i],f3[i]);
-        return f;
-    }
-    inline vector<ModInt<998244353>> convolution(const vector<ModInt<998244353>>& g,const vector<ModInt<998244353>>& h){return convolution_friendrymod<998244353>(g,h);}
-    inline vector<ModInt<1000000007>> convolution(const vector<ModInt<1000000007>>& g,const vector<ModInt<1000000007>>& h){return convolution_arbitrarymod(g,h);}
-    // inline vector<RuntimeModInt<runtime_mod>> convolution(const vector<RuntimeModInt<runtime_mod>>& g,const vector<RuntimeModInt<runtime_mod>>& h){return convolution_arbitrarymod(g,h);}
-
     static inline Mint nth_term_impl(long long n, Fps numerator,Fps denominator) {
         while(n) {
             numerator   *= denominator.minus_x();
@@ -79,12 +59,8 @@ template<class T> struct FormalPowerSeries : public vector<T> {
         }
         return numerator[0];
     }
-public:
     //a0 + a_1*x^1 + a_2*x^2 + ... + a_(n-1)*x^(n-1)
-    inline static Mint nth_term(long long n,const Fps& numerator,const Fps& denominator) {return nth_term_impl(n,numerator,denominator);}
     FormalPowerSeries(vector<Mint> v){*this=FormalPowerSeries(v.size());for(int i=0;i<v.size();++i) (*this)[i]=v[i];}
-    Fps operator*(const Fps& r) const { return Fps(*this) *= r; }
-    Fps &operator*=(const Fps& r) {return *this = convolution(*this,r);}
     Fps operator*(const int r) const {return Fps(*this) *= r; }
     Fps &operator*=(const int r) {for(int i=0;i< this->size(); ++i) (*this)[i] *= r; return *this; }
     Fps operator*(const long long int r) const {return Fps(*this) *= r; }
@@ -101,11 +77,12 @@ public:
     Fps &operator+=(const int r) {for(int i=0;i< this->size(); ++i) (*this)[i] += r; return *this; }
 
     Fps operator-(void) const {return Fps(*this) *= (-1);}
-    Fps operator-(const Fps& r) const { return Fps(*this) -= r; }
-    Fps &operator-=(const Fps& r) {if(r.size() > this->size()) this->resize(r.size());for(int i = 0; i < r.size(); i++) (*this)[i] -= r[i];return *this;}
     Fps operator-(const int r) const {return Fps(*this) -= r; }
     Fps &operator-=(const int r) {for(int i=0;i< this->size(); ++i) (*this)[i] -= r; return *this; }
-    Fps prefix(size_t n) const {return Fps(this->begin(),this->begin()+min(n,this->size()));}
+
+    Fps prefix(size_t n) const {
+        return Fps(this->begin(),this->begin()+min(n,this->size()));
+    }
     Fps pow(long long k,size_t n) const {
         Fps ret(n,0);
         for(size_t i=0; i < min(n,this->size()) && i*k < n; ++i) {
@@ -119,8 +96,6 @@ public:
         return ret;
     }
     Fps pow(long long k) const {return pow(k,this->size());}
-    Fps inv(size_t n) const {Fps ret({Mint(1)/(*this)[0]});for(size_t i=1;i < n; i<<=1) ret = (ret*2-ret*(ret*(this->prefix(i<<1))).prefix(i<<1)).prefix(i<<1);return ret.prefix(n);}
-    Fps inv(void) const {return inv(this->size());}
     Fps diff(void) const {Fps ret(max(0,int(this->size())-1));for(int i=0;i<ret.size(); ++i) ret[i]=(*this)[i+1]*(i+1);return ret;}
     Fps intg(void) const {Fps ret(this->size()+1);for(int i=1;i<ret.size(); ++i) ret[i]=(*this)[i-1]/i;return ret;}
     Fps log(size_t n) const {return (this->diff()*this->inv(n)).intg().prefix(n);}
