490dda9f61ef2403fb9268460719dbf86b530c88
ningenMe <amohkuru@gmail.com>
2021-04-26 09:18:50 +0900

    [modify]

--- lib/data-structure/convex-hull-trick/ConvexHullTrickMonotone.cpp
+++ lib/data-structure/convex-hull-trick/ConvexHullTrickMonotone.cpp
@@ -79,17 +79,3 @@ public:
 		lines.clear();
 	}
 };
-
-//最小値クエリ
-template<class T> struct ValueMin {
-	using TypeValue = T;
-	inline static constexpr TypeValue unit_value = 3e18;
-	inline static constexpr bool func_compare(TypeValue l,TypeValue r){return l<r;}
-};
-
-//最大値クエリ
-template<class T> struct ValueMax {
-	using TypeValue = T;
-	inline static constexpr TypeValue unit_value = -3e18;
-	inline static constexpr bool func_compare(TypeValue l,TypeValue r){return l>r;}
-};
