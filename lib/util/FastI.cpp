40b65aea3ef97d6992b12c4bd8cfafb3a1ef82e7
ningenMe <amohkuru@gmail.com>
2023-05-11 02:45:28 +0900

    [fix]

--- lib/util/FastI.cpp
+++ lib/util/FastI.cpp
@@ -7,10 +7,9 @@ private:
     inline static constexpr int ch_0='0';
     inline static constexpr int ch_9='9';
     template<typename T> inline static void readint(T &x) {
-        int neg=0; char ch; x=0;
-        ch=getchar();
+        int neg=0; char ch; x=0; ch=getchar();
         if(ch=='-') neg=1,ch=getchar();
-        for(;(ch_0 <= ch && ch <= ch_9); ch = getchar()) x = x*10 + (ch-ch_0);
+        for(;(ch_0 <= ch && ch <= ch_9); ch = getchar()) x = x*10 + (ch-ch_0); 
         if(neg) x*=-1;
     }
 public:
