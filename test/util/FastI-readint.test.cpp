d5c13378bd05b3a717ff4a1207876651bf620201
ningenMe <amohkuru@gmail.com>
2023-05-11 02:04:45 +0900

    [fix]

---
+++ test/util/FastI-readint.test.cpp
@@ -0,0 +1,15 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/many_aplusb"
+
+#include <iostream>
+using namespace std;
+#include "../../lib/util/FastI.cpp"
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int t; FastI::read(t);
+    while(t--) {
+        long long x,y; FastI::read(x);FastI::read(y);
+        cout << x+y << "\n";
+    }
+    return 0;
+}
