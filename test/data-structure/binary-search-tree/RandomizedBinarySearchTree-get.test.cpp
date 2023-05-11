bf38b77fa4615ce6ac806a187381b351a5497a44
ningenMe <amohkuru@gmail.com>
2023-05-12 03:08:31 +0900

    Revert "[delete] bst!!"
    This reverts commit 6e6e248cf66f54b48066530284382b548e4b8518.

---
+++ test/data-structure/binary-search-tree/RandomizedBinarySearchTree-get.test.cpp
@@ -0,0 +1,34 @@
+#define PROBLEM "https://yukicoder.me/problems/no/649"
+
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+using namespace std;
+#include "../../../lib/data-structure/binary-search-tree/RandomizedBinarySearchTree.cpp"
+#include "../../../lib/operator/monoid/MonoidRangeSumPointAdd.cpp"
+
+int main(){
+    cin.tie(0);ios::sync_with_stdio(false);
+	RandomizedBinarySearchTree<MonoidRangeSumPointAdd<long long>> st;
+	int Q,K; cin >> Q >> K;
+	while (Q--){
+		int q; cin >> q;
+		if(q==1){
+			long long x; cin >> x;
+			st.insert(x);
+		}
+		else{
+			long long x=-1;
+			if(st.size()>=K){
+				x=st.get(K-1);
+				st.erase(x);
+			}
+			cout << x << endl;
+		}
+	}
+
+    return 0;
+}
---
+++ test/data-structure/binary-search-tree/RandomizedBinarySearchTreeSequence-rmq.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
+
+#include <vector>
+#include <iostream>
+#include <cassert>
+#include <queue>
+using namespace std;
+#include "../../../lib/operator/monoid/MonoidRangeMinPointUpdate.cpp"
+#include "../../../lib/data-structure/binary-search-tree/RandomizedBinarySearchTreeSequence.cpp"
+
+int main(void){
+    cin.tie(0);ios::sync_with_stdio(false);
+    RandomizedBinarySearchTreeSequence<MonoidRangeMinPointUpdate<long long>> lbst;
+    int N,Q; cin >> N >> Q;
+    for(int i=0;i<N;++i) {
+        int a; cin >> a;
+        lbst.insert(i,a);
+    }
+    while(Q--) {
+        int l,r; cin >> l >> r;
+        cout << lbst.fold(l,r) << "\n";
+    }
+    return 0; 
+}
+
