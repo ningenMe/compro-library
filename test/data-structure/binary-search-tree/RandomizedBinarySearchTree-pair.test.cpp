4309c49beb63f42db9b7cd536863ce11c0c84be6
ningenMe <amohkuru@gmail.com>
2023-05-12 02:44:17 +0900

    Revert "[delete]"
    This reverts commit e209264fbb7b7cba6e2cd8a3da953a9a46bc7ae1.

---
+++ test/data-structure/binary-search-tree/RandomizedBinarySearchTree-pair.test.cpp
@@ -0,0 +1,35 @@
+#define PROBLEM "https://yukicoder.me/problems/no/822"
+
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+using namespace std;
+#include "../../../lib/data-structure/binary-search-tree/RandomizedBinarySearchTree.cpp"
+
+template<class T> struct Monoid {
+	using TypeNode = T;
+	inline static constexpr TypeNode unit_node = {0,0};
+	inline static constexpr TypeNode func_fold(TypeNode l,TypeNode r){return {0,0};}
+};
+
+int main() {
+    int N,K; cin >> N >> K;
+    if(N+1 <= K){
+		cout << "INF" << endl;
+		return 0;
+	}
+    RandomizedBinarySearchTree<Monoid<pair<int,int>>> st;
+    for(int i = 0; i < (1<<20); ++i) {
+        if((i&N) != N) continue;
+        for(int j = -K; j <= K; ++j) {
+            int a = i, b = i + j;
+            if(a>b) swap(a,b);
+            if(0<=b && b-a<=K && ((a&b)==N) && !st.count({a,b})) st.insert({a,b});
+        }
+    }
+    cout << st.size() << endl;
+    return 0;
+}
---
+++ test/data-structure/segment-tree/SegmentTree-suffix-binary-search.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://yukicoder.me/problems/4072"
+
+#include <vector>
+#include <iostream>
+#include <cassert>
+using namespace std;
+#include "../../../lib/data-structure/segment-tree/SegmentTree.cpp"
+#include "../../../lib/math/Gcd.cpp"
+#include "../../../lib/operator/monoid/MonoidRangeGcdPointUpdate.cpp"
+
+// solution by binary search in prefix range on segment tree 
+int main() {
+	cin.tie(0);ios::sync_with_stdio(false);
+	long long N; cin >> N;
+	vector<long long> A(N);
+	for(int i = 0; i < N; ++i) cin >> A[i];
+	SegmentTree<MonoidRangeGcdPointUpdate<long long>> seg(A);
+	long long ans = 0;
+	for(int i = N-1; 0 <= i; --i) {
+		ans += seg.suffix_binary_search(-1,i,1) + 1;
+	}
+	cout << ans << endl;
+}
