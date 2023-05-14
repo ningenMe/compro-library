d145538ba5121d81b2c34f8096a96ed6c8c88d66
ningenMe <amohkuru@gmail.com>
2023-05-15 02:26:08 +0900

    [fix]

--- test/graph/UnionFindTree.test.cpp
+++ test/graph/UnionFindTree.test.cpp
@@ -4,14 +4,19 @@
 #include <iostream>
 #include <numeric>
 using namespace std;
+#include "../../lib/util/FastIO.cpp"
 #include "../../lib/graph/UnionFindTree.cpp"
 
+
 int main(){
-    int N,Q; cin >> N >> Q;
+    cin.tie(0)->sync_with_stdio(0);
+    int N,Q; 
+    read(N); read(Q);
     UnionFindTree uf(N);
     while(Q--){
-        int q,a,b; cin >> q >> a >> b;
-        if(q) cout << uf.connected(a,b) << endl;
+        int q,a,b; 
+        read(q);read(a);read(b);
+        if(q) cout << uf.connected(a,b) << "\n";
         else uf.merge(a,b);
     }
 }
