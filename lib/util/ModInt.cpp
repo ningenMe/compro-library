be90ac275a7d29469bf22a019489e0c128cab3ef
ningenMe <amohkuru@gmail.com>
2023-05-18 00:39:49 +0900

    [fix] modint

--- lib/util/ModInt.cpp
+++ lib/util/ModInt.cpp
@@ -7,36 +7,38 @@ public:
     long long x;
     constexpr ModInt():x(0) {}
     constexpr ModInt(long long y) : x(y>=0?(y%mod): (mod - (-y)%mod)%mod) {}
-    ModInt &operator+=(const ModInt &p) {if((x += p.x) >= mod) x -= mod;return *this;}
-    ModInt &operator+=(const long long y) {ModInt p(y);if((x += p.x) >= mod) x -= mod;return *this;}
-    ModInt &operator+=(const int y) {ModInt p(y);if((x += p.x) >= mod) x -= mod;return *this;}
-    ModInt &operator-=(const ModInt &p) {if((x += mod - p.x) >= mod) x -= mod;return *this;}
-    ModInt &operator-=(const long long y) {ModInt p(y);if((x += mod - p.x) >= mod) x -= mod;return *this;}
-    ModInt &operator-=(const int y) {ModInt p(y);if((x += mod - p.x) >= mod) x -= mod;return *this;}
-    ModInt &operator*=(const ModInt &p) {x = (x * p.x % mod);return *this;}
-    ModInt &operator*=(const long long y) {ModInt p(y);x = (x * p.x % mod);return *this;}
-    ModInt &operator*=(const int y) {ModInt p(y);x = (x * p.x % mod);return *this;}
-    ModInt &operator^=(const ModInt &p) {x = (x ^ p.x) % mod;return *this;}
-    ModInt &operator^=(const long long y) {ModInt p(y);x = (x ^ p.x) % mod;return *this;}
-    ModInt &operator^=(const int y) {ModInt p(y);x = (x ^ p.x) % mod;return *this;}
-    ModInt &operator/=(const ModInt &p) {*this *= p.inv();return *this;}
-    ModInt &operator/=(const long long y) {ModInt p(y);*this *= p.inv();return *this;}
-    ModInt &operator/=(const int y) {ModInt p(y);*this *= p.inv();return *this;}
-    ModInt operator=(const int y) {ModInt p(y);*this = p;return *this;}
-    ModInt operator=(const long long y) {ModInt p(y);*this = p;return *this;}
-    ModInt operator-() const {return ModInt(-x); }
-    ModInt operator++() {x++;if(x>=mod) x-=mod;return *this;}
-    ModInt operator--() {x--;if(x<0) x+=mod;return *this;}
-    ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
-    ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
-    ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
-    ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
-    ModInt operator^(const ModInt &p) const { return ModInt(*this) ^= p; }
-    bool operator==(const ModInt &p) const { return x == p.x; }
-    bool operator!=(const ModInt &p) const { return x != p.x; }
-    ModInt inv() const {int a=x,b=mod,u=1,v=0,t;while(b > 0) {t = a / b;swap(a -= t * b, b);swap(u -= t * v, v);} return ModInt(u);}
-    ModInt pow(long long n) const {ModInt ret(1), mul(x);for(;n > 0;mul *= mul,n >>= 1) if(n & 1) ret *= mul;return ret;}
+    constexpr ModInt &operator+=(const ModInt &p) {if((x += p.x) >= mod) x -= mod;return *this;}
+    constexpr ModInt &operator+=(const long long y) {ModInt p(y);if((x += p.x) >= mod) x -= mod;return *this;}
+    constexpr ModInt &operator+=(const int y) {ModInt p(y);if((x += p.x) >= mod) x -= mod;return *this;}
+    constexpr ModInt &operator-=(const ModInt &p) {if((x += mod - p.x) >= mod) x -= mod;return *this;}
+    constexpr ModInt &operator-=(const long long y) {ModInt p(y);if((x += mod - p.x) >= mod) x -= mod;return *this;}
+    constexpr ModInt &operator-=(const int y) {ModInt p(y);if((x += mod - p.x) >= mod) x -= mod;return *this;}
+    constexpr ModInt &operator*=(const ModInt &p) {x = (x * p.x % mod);return *this;}
+    constexpr ModInt &operator*=(const long long y) {ModInt p(y);x = (x * p.x % mod);return *this;}
+    constexpr ModInt &operator*=(const int y) {ModInt p(y);x = (x * p.x % mod);return *this;}
+    constexpr ModInt &operator^=(const ModInt &p) {x = (x ^ p.x) % mod;return *this;}
+    constexpr ModInt &operator^=(const long long y) {ModInt p(y);x = (x ^ p.x) % mod;return *this;}
+    constexpr ModInt &operator^=(const int y) {ModInt p(y);x = (x ^ p.x) % mod;return *this;}
+    constexpr ModInt &operator/=(const ModInt &p) {*this *= p.inv();return *this;}
+    constexpr ModInt &operator/=(const long long y) {ModInt p(y);*this *= p.inv();return *this;}
+    constexpr ModInt &operator/=(const int y) {ModInt p(y);*this *= p.inv();return *this;}
+    constexpr ModInt operator=(const int y) {ModInt p(y);*this = p;return *this;}
+    constexpr ModInt operator=(const long long y) {ModInt p(y);*this = p;return *this;}
+    constexpr ModInt operator-() const {return ModInt(-x); }
+    constexpr ModInt operator++() {x++;if(x>=mod) x-=mod;return *this;}
+    constexpr ModInt operator--() {x--;if(x<0) x+=mod;return *this;}
+    constexpr ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
+    constexpr ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
+    constexpr ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
+    constexpr ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
+    constexpr ModInt operator^(const ModInt &p) const { return ModInt(*this) ^= p; }
+    constexpr bool operator==(const ModInt &p) const { return x == p.x; }
+    constexpr bool operator!=(const ModInt &p) const { return x != p.x; }
+    // ModInt inv() const {int a=x,b=mod,u=1,v=0,t;while(b > 0) {t = a / b;swap(a -= t * b, b);swap(u -= t * v, v);} return ModInt(u);}
+    constexpr ModInt inv() const {int a=x,b=mod,u=1,v=0,t=0,tmp=0;while(b > 0) {t = a / b;a-=t*b;tmp=a;a=b;b=tmp;u-=t*v;tmp=u;u=v;v=tmp;} return ModInt(u);}
+    constexpr ModInt pow(long long n) const {ModInt ret(1), mul(x);for(;n > 0;mul *= mul,n >>= 1) if(n & 1) ret *= mul;return ret;}
     friend ostream &operator<<(ostream &os, const ModInt &p) {return os << p.x;}
     friend istream &operator>>(istream &is, ModInt &a) {long long t;is >> t;a = ModInt<mod>(t);return (is);}
 };
-//using modint = ModInt<MOD>;
+constexpr long long MOD_998244353 = 998244353;
+constexpr long long MOD_1000000007 = 1'000'000'000LL + 7; //'
