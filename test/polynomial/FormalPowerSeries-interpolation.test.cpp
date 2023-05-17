d1dca420faed75fd6f4418d11cb54717e3e451e2
ningenMe <amohkuru@gmail.com>
2023-05-18 02:51:13 +0900

    [fix]

--- test/polynomial/FormalPowerSeries-interpolation.test.cpp
+++ test/polynomial/FormalPowerSeries-interpolation.test.cpp
@@ -7,19 +7,25 @@
 #include <array>
 
 using namespace std;
+
+#include "../../lib/util/FastIO.cpp"
 #include "../../lib/util/ModInt.cpp"
-#include "../../lib/math/NumberTheoreticalTransform.cpp"
-#include "../../lib/math/FormalPowerSeries.cpp"
-constexpr long long MOD = 998244353;
-using fps = FormalPowerSeries<ModInt<MOD>>;
+#include "../../lib/convolution/NumberTheoreticalTransform.cpp"
+#include "../../lib/polynomial/FormalPowerSeries.cpp"
 
 int main() {
     cin.tie(0);ios::sync_with_stdio(false);
-    int N; cin >> N;
-    vector<ModInt<MOD>> X(N),Y(N);
-    for(int i=0;i<N;++i) cin >> X[i];
-    for(int i=0;i<N;++i) cin >> Y[i];
-    auto f = fps::interpolation(X,Y);
+    int N; read(N);
+    vector<ModInt<MOD_998244353>> X(N),Y(N);
+    for(int i=0;i<N;++i) {
+        int a; read(a);
+        X[i]=a;
+    }
+    for(int i=0;i<N;++i) {
+        int a; read(a);
+        Y[i]=a;
+    }
+    auto f = FormalPowerSeries<MOD_998244353>::interpolation(X,Y);
     for(int i=0;i<N;++i) cout << f[i] << " \n"[i==N-1];
     return 0;
 }
