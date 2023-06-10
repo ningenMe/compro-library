776b895787845cd9fa8bf142cc1c9808d9788be9
ningenMe <amohkuru@gmail.com>
2023-06-10 18:11:26 +0900

    [fix]

--- lib/13-static-range-query/WaveletMatrix.cpp
+++ lib/13-static-range-query/WaveletMatrix.cpp
@@ -1,41 +1,52 @@
 /*
  * @title WaveletMatrix
- * @docs md/data-structure/WaveletMatrix.md
+ * @docs md/static-range-query/WaveletMatrix.md
  */
 template<class T> class WaveletMatrix{
     using u64 = unsigned long long;
     using u32 = unsigned int;
+
     class BitVector{
         inline static constexpr size_t BIT_BLOCK_SIZE = 5;
-        inline static constexpr size_t BIT_BLOCK_NUM  = 1<<BIT_BLOCK_SIZE;
-        inline static constexpr u32 popcount(u32 x) {return __builtin_popcount(x);}
-        inline static constexpr u32 popcount(u64 x) {return __builtin_popcountll(x);}
-        vector<u32> vec, acc;
+        inline static constexpr size_t BIT_BLOCK_NUM  = (1<<BIT_BLOCK_SIZE) - 1; //31
+        vector<u32> ar, acc;
+        u32 popcount(u32 x) const {return __builtin_popcount(x);}
+        u32 length;
     public:
-        BitVector(const u32 N) {
-            u32 tmp = (N + BIT_BLOCK_NUM-1) / BIT_BLOCK_NUM;
-            vec.assign(tmp, 0);
+        u32 sum_bit0;
+        BitVector(const u32 N): length(N) {
+            u32 tmp = (N + BIT_BLOCK_NUM) / (BIT_BLOCK_NUM+1);
+            ar.assign(tmp, 0);
             acc.assign(tmp, 0);
         }
-        void build() { for (size_t i = 0,sum = 0; i < acc.size(); ++i) acc[i] = (sum += popcount(vec[i]) );}
+        void build() { 
+            for (size_t i = 0,sum = 0; i < acc.size(); ++i) acc[i] = (sum += popcount(ar[i]) );
+            sum_bit0 = rank(length, 0);
+        }
         //[0,r) count of bit
         u32 rank(const u32 r, const bool bit) const {
             u32 bit_r = r >> BIT_BLOCK_SIZE; 
-            u32 sum = (bit_r ? acc[bit_r - 1] : 0) + popcount(vec[bit_r] & ((1U << (r & (BIT_BLOCK_NUM-1))) - 1));
+            u32 sum = (bit_r ? acc[bit_r - 1] : 0) + popcount(ar[bit_r] & ((1U << (r & BIT_BLOCK_NUM)) - 1));
             return bit ? sum : r - sum;
         }
         //[l,l+1) = bit
         void update(const u32 l, const bool bit) {
             u32 bit_l = l >> BIT_BLOCK_SIZE;
-            if (bit) vec[bit_l] |=   1U << (l & (BIT_BLOCK_NUM-1));
-            else     vec[bit_l] &= ~(1U << (l & (BIT_BLOCK_NUM-1)));
+            if (bit) ar[bit_l] |=   1U << (l & BIT_BLOCK_NUM);
+            else     ar[bit_l] &= ~(1U << (l & BIT_BLOCK_NUM));
         }
         //[l,l+1)
         bool operator[](const u32 l) const {
-            return ((vec[l >> BIT_BLOCK_SIZE] >> (l & (BIT_BLOCK_NUM-1))) & 1);
+            return ((ar[l >> BIT_BLOCK_SIZE] >> (l & BIT_BLOCK_NUM)) & 1);
         }
     };
-    vector<u32> Zarts(const vector<T>& ar) {
+    size_t length;
+    size_t depth;
+    vector<BitVector> bit_vector;
+    vector<T> ar;
+    void build() {
+        length = ar.size();    
+        if(!length) return;
         vector<u32> ord(ar.size()),compressed(ar.size());
         iota(ord.begin(),ord.end(),0);
         sort(ord.begin(),ord.end(),[&](size_t l, size_t r){return ar[l]<ar[r];});
@@ -48,78 +59,81 @@ template<class T> class WaveletMatrix{
             compressed[j] = cnt;
             pre = ar[j];
         }
-        return compressed;
-    }
-    size_t length;
-    size_t depth;
-    vector<BitVector> multi_bit_vector;
-    vector<u32> sum_bit_off;
-    vector<T> vec;
+        sort(ar.begin(),ar.end());
+        ar.erase(unique(ar.begin(),ar.end()),ar.end());
 
-    inline T get_impl(u32 l) const {
-        u32 val = 0;
-        for (u32 j = 0; j < depth; ++j) {
-            const bool bit = multi_bit_vector[j][l];
-            val = ((val << 1) | bit);
-            l = multi_bit_vector[j].rank(l, bit);
-            if(bit) l += sum_bit_off[j];
+        for(depth=0; (1<<depth) <= cnt+2; ++depth );
+        bit_vector.assign(depth, BitVector(length));
+        vector<u32> prev = compressed, next = prev;
+        for(u32 d = 0; d < depth; ++d,swap(prev,next)) {
+            u32 bit = 1UL << (depth - d - 1);
+            u32 idx_bit0 = 0, idx_bit1 = 0;
+            for(u32 i = 0; i < length; ++i) idx_bit1 += !(prev[i] & bit);
+            for(u32 i = 0; i < length; ++i) {
+                if (prev[i] & bit) bit_vector[d].update(i,1), next[idx_bit1++] = prev[i];
+                else next[idx_bit0++] = prev[i];
+            }
+            bit_vector[d].build();
         }
-        return vec[val];
+    }
+    inline T get_impl(u32 l) const {
+        u32 key = 0;
+        for (u32 d = 0; d < depth; ++d) {
+            const bool bit = bit_vector[d][l];
+            key = ((key << 1) | bit);
+            l = bit_vector[d].rank(l, bit);
+            if(bit) l += bit_vector[d].sum_bit0;
+        }
+        return ar[key];
     }
     inline T range_kth_smallest_impl(u32 l, u32 r, u32 k) const {
-        u32 val = 0;
-        for (u32 j = 0; j < depth; ++j) {
-            u32 cnt_bit_off = multi_bit_vector[j].rank(r, 0) - multi_bit_vector[j].rank(l, 0);
+        u32 key = 0;
+        for (u32 d = 0; d < depth; ++d) {
+            u32 cnt_bit_off = bit_vector[d].rank(r, 0) - bit_vector[d].rank(l, 0);
             const bool bit = (k >= cnt_bit_off);
-            val = ((val << 1) | bit);
-            l = multi_bit_vector[j].rank(l, bit);
-            r = multi_bit_vector[j].rank(r, bit);
-            if (bit) l += sum_bit_off[j], r += sum_bit_off[j], k -= cnt_bit_off;
+            key = ((key << 1) | bit);
+            l = bit_vector[d].rank(l, bit);
+            r = bit_vector[d].rank(r, bit);
+            if (bit) l += bit_vector[d].sum_bit0, r += bit_vector[d].sum_bit0, k -= cnt_bit_off;
         }
-        return vec[val];
+        return ar[key];
     }
-    void build() {
-        length = vec.size();
-        auto compressed = Zarts(vec);
-        sort(vec.begin(),vec.end());
-        vec.erase(unique(vec.begin(),vec.end()),vec.end());
-        u32 maxi = *max_element(compressed.begin(),compressed.end()) + 1;
-        for(depth=0; (1<<depth) <= maxi; ++depth );
-
-        multi_bit_vector.assign(depth, BitVector(length));
-        sum_bit_off.assign(depth, 0UL);
-        vector<u32> prev = compressed, next = prev;
-        for(u32 j = 0; j < depth; ++j,swap(prev,next)) {
-            u32 bit = 1UL << (depth - j - 1);
-            for(u32 i = 0; i < length; ++i) sum_bit_off[j] += !(prev[i] & bit);
-            u32 idx_bit_off = 0, idx_bit_on = sum_bit_off[j];
-            for(u32 i = 0; i < length; ++i) {
-                if (prev[i] & bit) multi_bit_vector[j].update(i,1), next[idx_bit_on++] = prev[i];
-                else next[idx_bit_off++] = prev[i];
+    inline u32 range_freq_upper_impl(u32 l, u32 r, T upper) const {
+        if(!length) return 0;
+        if(ar.back() < upper) return (r-l);
+        if(r-l<=0) return 0;
+        auto itr = lower_bound(ar.begin(),ar.end(),upper);
+        u32 key = (itr - ar.begin());
+        u32 cnt=0;
+        for (u32 d = 0; d < depth; ++d) {
+            u32 bit = (key >> (depth - d - 1)) & 1U;
+            u32 tl = bit_vector[d].rank(l,0);
+            u32 tr = bit_vector[d].rank(r,0);
+            if(bit) {
+                cnt += tr-tl;
+                l += bit_vector[d].sum_bit0-tl;
+                r += bit_vector[d].sum_bit0-tr;
             }
-            multi_bit_vector[j].build();
+            else {
+                l = tl;
+                r = tr;
+            }
         }
+        return cnt;
     }
-    //[l,r) range count of more and less than c (c'<c, c'=c, c<c')
-    // tuple<uint32, uint32, uint32> rank_all(T c, uint32 l, uint32 r) const
-    // {
-    // 	if (c > vec.back()) return make_tuple(r - l, 0U, 0U);
-    // 	array<uint32,2> more_and_less = {0,0};
-    // 	for (uint32 j = 0; j < depth; ++j) {
-    // 		const bool bit = (c >> (depth - j - 1)) & 1;
-    // 		more_and_less[bit] += r - l;
-    // 		l = multi_bit_vector[j].rank(l, bit);
-    // 		r = multi_bit_vector[j].rank(r, bit);
-    // 		if (bit) l += sum_bit_off[j], r += sum_bit_off[j];
-    // 		more_and_less[bit] -= r - l;
-    // 	}
-    // 	return make_tuple(more_and_less[1], r - l, more_and_less[0]);
-    // }
+
 public:
-    WaveletMatrix(const vector<T> &vec): vec(vec) { build(); }
+    WaveletMatrix(const vector<T> &ar): ar(ar) { build(); }
     //[l,l+1) element
     inline T get(const u32 l) const {return get_impl(l);}
     //[l,r) range k (0-indexed) th smallest number
     T range_kth_smallest(const u32 l, const u32 r, const u32 k) const {return range_kth_smallest_impl(l,r,k);}
+    //[l,r) range k (0-indexed) th largest number
     T range_kth_largest(const u32 l, const u32 r, const u32 k) const {return range_kth_smallest_impl(l,r,(r-l)-(k+1));}
+    //[l,r) range freq of val (val < upper)
+    u32 range_freq_upper(const u32 l, const u32 r, const T upper) const {return range_freq_upper_impl(l,r,upper);}
+    //[l,r) range freq of val (lower <= val < upper)
+    u32 range_freq_lower_upper(const u32 l, const u32 r, const T lower, const T upper) const {return range_freq_upper_impl(l,r,upper) - range_freq_upper_impl(l,r,lower);}
+    //[l,r) range freq of val
+    u32 range_freq(const u32 l, const u32 r, const T val) const {return range_freq_lower_upper(l,r,val,val+1);}
 };
